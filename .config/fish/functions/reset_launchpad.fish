a19a0c10d4f809bf7c43141e96397b149b68f61e
Léana 江 <leana.jiang@icloud.com>
2022-12-21 13:08:09 +0100

    fixed description

--- .config/fish/functions/reset_launchpad.fish
+++ .config/fish/functions/reset_launchpad.fish
@@ -1,3 +1,4 @@
-function reset_launchpad --wraps='defaults write com.apple.dock ResetLaunchPad -bool true; killall Dock' --description 'alias reset_launchpad=defaults write com.apple.dock ResetLaunchPad -bool true; killall Dock'
-  defaults write com.apple.dock ResetLaunchPad -bool true; killall Dock $argv; 
+function reset_launchpad --wraps='defaults write com.apple.dock ResetLaunchPad -bool true; killall Dock' --description 'Reset launchpad disposition'
+    defaults write com.apple.dock ResetLaunchPad -bool true
+    killall Dock
 end
