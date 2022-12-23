0ba4cdc4a9dcb314eabfb6ca49f8da9bdc919430
Léana 江 <leana.jiang@icloud.com>
2022-12-23 12:22:47 +0100

    added upload.fish

---
+++ .config/fish/functions/upload.fish
@@ -0,0 +1,10 @@
+function upload
+    if count $argv >/dev/null
+        rsync -r -v --chown=caddy:caddy --chmod=u=rX,g=rX,o=rX --exclude ".DS_Store" --update --times --compress --delay-updates \
+            $argv \
+            earth2077.fr:/srv/http/share/
+        echo -e "\nhttps://earth2077.fr/share/"(path basename $argv)
+    else
+        echo "Please append path to a file to be uploaded"
+    end
+end
