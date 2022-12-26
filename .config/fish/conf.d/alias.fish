2ab520fc05886195026ef48a19d83a4f1b61f13a
Léana 江 <leana.jiang@icloud.com>
2022-12-26 23:12:01 +0100

    feat(functions): added util functions for darwin

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -2,7 +2,12 @@ alias ls='tree -Cph -L 1'
 alias la='tree -Cpha -L 1'
 alias history='history | nl | less +G'
 
-if not uname -a | grep -qi "darwin"
-	alias chmod='chmod --preserve-root'
-	alias chown='chown --preserve-root'
+
+if not uname -a | grep -qi darwin
+    alias chmod='chmod --preserve-root'
+    alias chown='chown --preserve-root'
+else
+    alias hide_desktop='defaults write com.apple.finder CreateDesktop false; killall Finder'
+    alias show_desktop='defaults write com.apple.finder CreateDesktop true; killall Finder'
+    alias reset_launchpad='defaults write com.apple.dock ResetLaunchPad -bool true; killall Dock'
 end
--- .config/fish/functions/reset_launchpad.fish
+++
@@ -1,4 +0,0 @@
-function reset_launchpad --wraps='defaults write com.apple.dock ResetLaunchPad -bool true; killall Dock' --description 'Reset launchpad disposition'
-    defaults write com.apple.dock ResetLaunchPad -bool true
-    killall Dock
-end
