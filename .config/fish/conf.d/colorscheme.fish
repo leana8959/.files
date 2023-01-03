a9ef521f4e3d2429c8fcd74612eef7ab14a6e6ca
Léana 江 <leana.jiang@icloud.com>
2023-01-03 08:55:06 +0100

    fix(colorscheme): variable scope

--- .config/fish/conf.d/colorscheme.fish
+++ .config/fish/conf.d/colorscheme.fish
@@ -1,38 +1,38 @@
-set -U fish_color_normal normal
-set -U fish_color_command ffffff
-set -U fish_color_quote ffd75f
-set -U fish_color_redirection ff005f --bold
-set -U fish_color_end ffd7af
-set -U fish_color_error brred
-set -U fish_color_param d7d7ff
-set -U fish_color_comment 00afff
-set -U fish_color_match --background=brblue
-set -U fish_color_selection white --bold --background=brblack
-set -U fish_color_search_match bryellow --background=brblack
-set -U fish_color_history_current --bold
-set -U fish_color_operator brcyan
-set -U fish_color_escape brcyan
-set -U fish_color_cwd green
-set -U fish_color_cwd_root red
-set -U fish_color_valid_path --underline
-set -U fish_color_autosuggestion 878787
-set -U fish_color_user brgreen
-set -U fish_color_host normal
-set -U fish_color_cancel --reverse
-set -U fish_pager_color_background
-set -U fish_pager_color_prefix normal --bold --underline
-set -U fish_pager_color_progress brwhite --background=cyan
-set -U fish_pager_color_completion normal
-set -U fish_pager_color_description B3A06D --italics
-set -U fish_pager_color_selected_background --reverse
-set -U fish_pager_color_selected_prefix
-set -U fish_pager_color_selected_completion
-set -U fish_pager_color_selected_description
-set -U fish_pager_color_secondary_prefix
-set -U fish_color_keyword
-set -U fish_pager_color_secondary_description
-set -U fish_pager_color_secondary_background
-set -U fish_color_host_remote
-set -U fish_pager_color_secondary_completion
-set -U fish_color_option
+set -gx fish_color_normal normal
+set -gx fish_color_command ffffff
+set -gx fish_color_quote ffd75f
+set -gx fish_color_redirection ff005f --bold
+set -gx fish_color_end ffd7af
+set -gx fish_color_error brred
+set -gx fish_color_param d7d7ff
+set -gx fish_color_comment 00afff
+set -gx fish_color_match --background=brblue
+set -gx fish_color_selection white --bold --background=brblack
+set -gx fish_color_search_match bryellow --background=brblack
+set -gx fish_color_history_current --bold
+set -gx fish_color_operator brcyan
+set -gx fish_color_escape brcyan
+set -gx fish_color_cwd green
+set -gx fish_color_cwd_root red
+set -gx fish_color_valid_path --underline
+set -gx fish_color_autosuggestion 878787
+set -gx fish_color_user brgreen
+set -gx fish_color_host normal
+set -gx fish_color_cancel --reverse
+set -gx fish_pager_color_background
+set -gx fish_pager_color_prefix normal --bold --underline
+set -gx fish_pager_color_progress brwhite --background=cyan
+set -gx fish_pager_color_completion normal
+set -gx fish_pager_color_description B3A06D --italics
+set -gx fish_pager_color_selected_background --reverse
+set -gx fish_pager_color_selected_prefix
+set -gx fish_pager_color_selected_completion
+set -gx fish_pager_color_selected_description
+set -gx fish_pager_color_secondary_prefix
+set -gx fish_color_keyword
+set -gx fish_pager_color_secondary_description
+set -gx fish_pager_color_secondary_background
+set -gx fish_color_host_remote
+set -gx fish_pager_color_secondary_completion
+set -gx fish_color_option
 
