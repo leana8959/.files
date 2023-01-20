531bc4c026f38245cba0be8c59af3eac66fdd9e5
Léana 江 <leana.jiang@icloud.com>
2023-01-20 14:10:21 +0100

    fix locale

--- .config/fish/conf.d/locale.fish
+++ .config/fish/conf.d/locale.fish
@@ -1,15 +1,4 @@
 set -gx LANG zh_TW.UTF-8
 set -gx LANGUAGE zh_TW
-set -gx LC_CTYPE zh_TW.UTF-8
-set -gx LC_NUMERIC zh_TW.UTF-8
-set -gx LC_TIME zh_TW.UTF-8
-set -gx LC_COLLATE zh_TW.UTF-8
-set -gx LC_MONETARY zh_TW.UTF-8
-set -gx LC_MESSAGES zh_TW.UTF-8
-set -gx LC_PAPER zh_TW.UTF-8
-set -gx LC_NAME zh_TW.UTF-8
-set -gx LC_ADDRESS zh_TW.UTF-8
-set -gx LC_TELEPHONE zh_TW.UTF-8
-set -gx LC_MEASUREMENT zh_TW.UTF-8
-set -gx LC_IDENTIFICATION zh_TW.UTF-8
+set -gx LC_ALL zh_TW.UTF-8
 
