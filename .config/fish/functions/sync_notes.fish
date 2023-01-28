7ad28790b500f54a3cf1e17d7ae442f8ffd97bb5
Léana 江 <leana.jiang@icloud.com>
2023-01-29 00:23:11 +0100

    fix(function): fixed permission

--- .config/fish/functions/sync_notes.fish
+++ .config/fish/functions/sync_notes.fish
@@ -1,6 +1,6 @@
 function sync_notes
     echo ===== (date) =====
-    rsync -r -v --chown=caddy:caddy --chmod=u=rX,g=rX,o=rX --exclude ".DS_Store" --update --times --compress --delay-updates \
+    rsync -r -v --chown=caddy:caddy --chmod=u=rwX,g=rX,o=rX --exclude ".DS_Store" --update --times --compress --delay-updates \
         /Users/leana/Documents/Rennes\ 1/L2/Prise\ de\ notes/ \
         earth2077.fr:/srv/http/notes/
 end
