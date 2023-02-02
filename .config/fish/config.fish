9f06b5593586193917b14fe81e5e8ea78baecf2a
Léana 江 <leana.jiang@icloud.com>
2023-02-02 20:53:14 +0100

    fix(fzf): horizontal split

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -gx GPG_TTY (tty)
+    # Default setting for fzf plugin
+    set -gx FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*" --preview-window="bottom"'
 end
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
