621995cdfed459bf13af5db488fbe489c6b80212
Léana 江 <leana.jiang@icloud.com>
2023-02-05 12:53:04 +0100

    feat(function): auto append date when upload files

---
+++ .config/fish/functions/file_extension.fish
@@ -0,0 +1,8 @@
+function file_extension --description Obtain\ file\'s\ extension
+    if count $argv >/dev/null
+        echo (basename $argv) | sed 's/.*\(\.[^.]*$\)/\1/'
+    else 
+        echo "Please supply a path"
+        return 1
+    end
+end
--- .config/fish/functions/upload.fish
+++ .config/fish/functions/upload.fish
@@ -1,10 +1,14 @@
 function upload
     if count $argv >/dev/null
+        set -f dest_name (file_mantissa $argv)"_"(timestamp)(file_extension $argv)
+
         rsync -r -v --chown=caddy:caddy --chmod=u=rwX,g=rX,o=rX --exclude ".DS_Store" --update --times --compress --delay-updates \
             $argv \
-            earth2077.fr:/srv/http/share/
-        echo -e "\nhttps://earth2077.fr/share/"(path basename $argv)
+            earth2077.fr:/srv/http/share/$dest_name
+
+        echo "https://earth2077.fr/share/"$dest_name
     else
         echo "Please append path to a file to be uploaded"
+        return 1
     end
 end
