c4f8feda2eb255280a3203283e6520d02afdc25f
Léana 江 <leana.jiang@icloud.com>
2023-02-05 14:01:49 +0100

    set locale to French

--- .config/fish/conf.d/locale.fish
+++ .config/fish/conf.d/locale.fish
@@ -1,4 +1,8 @@
-set -gx LANG zh_TW.UTF-8
-set -gx LANGUAGE zh_TW
-set -gx LC_ALL zh_TW.UTF-8
+# set -gx LANG zh_TW.UTF-8
+# set -gx LANGUAGE zh_TW
+# set -gx LC_ALL zh_TW.UTF-8
+
+set -gx LANG fr_FR.UTF-8
+set -gx LANGUAGE fr_FR
+set -gx LC_ALL fr_FR.UTF-8
 
