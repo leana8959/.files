2ee34bc7ec8c3271c2c720e21a8d540036b5204b
Léana 江 <leana.jiang@icloud.com>
2023-02-20 12:13:33 +0100

    add(nvim): nvim config rewrite (wip)

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -41,7 +41,7 @@ require('gitsigns').setup {
   },
 }
 
-require'nvim-treesitter.configs'.setup {
+require('nvim-treesitter.configs').setup {
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
   ensure_installed = { "c", "cpp", "lua", "vim", "help", "rust", "scala", "python"},
 
