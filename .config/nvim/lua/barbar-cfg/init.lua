e74e96a4d23298ff24e0eed910a4b4887d82ad2e
Léana 江 <leana.jiang@icloud.com>
2023-02-23 13:11:46 +0100

    add(nvim): barbar

---
+++ .config/nvim/lua/barbar-cfg/init.lua
@@ -0,0 +1,34 @@
+vim.g.bufferline = {
+	animation = true,
+	tabpages = true,
+	clickable = true,
+	icon_pinned = '',
+}
+
+
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+-- Move to previous/next
+map('n', 'gT', '<Cmd>BufferPrevious<CR>', opts)
+map('n', 'gt', '<Cmd>BufferNext<CR>', opts)
+map('n', '<A-{>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<A-}>', '<Cmd>BufferNext<CR>', opts)
+
+-- Re-order to previous/next
+map('n', '<C-{>', '<Cmd>BufferMovePrevious<CR>', opts)
+map('n', '<C-}>', '<Cmd>BufferMoveNext<CR>', opts)
+
+-- Goto buffer in position...
+map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+
+-- Pin/unpin buffer
+map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
