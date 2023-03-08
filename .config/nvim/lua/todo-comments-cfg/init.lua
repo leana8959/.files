def3a006f896b969cf596d59e28e98102462eec7
Léana 江 <leana.jiang@icloud.com>
2023-03-08 23:26:24 +0100

    fix(nvim): TODO comments background color
    Signed-off-by: Chiang Léana <leana.chiang@etudiant.univ-rennes1.fr>

--- .config/nvim/lua/todo-comments-cfg/init.lua
+++ .config/nvim/lua/todo-comments-cfg/init.lua
@@ -31,7 +31,7 @@ require "todo-comments".setup({
 		multiline_context = 8, -- extra lines that will be re-evaluated when changing a line
 		before = "", -- "fg" or "bg" or empty
 		keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
-		after = "fg", -- "fg" or "bg" or empty
+		after = "bg", -- "fg" or "bg" or empty
 		pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlightng (vim regex)
 		comments_only = true, -- uses treesitter to match keywords in comments only
 		max_line_len = 400, -- ignore lines longer than this
