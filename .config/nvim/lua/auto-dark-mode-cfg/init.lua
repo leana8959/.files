d413d2a8ccb37c52b81dcef5a6c7bff75011cbb8
Léana 江 <leana.jiang@icloud.com>
2023-03-09 18:51:19 +0100

    fix(nvim): fixed race condition in color initialization
    Signed-off-by: Chiang Léana <leana.chiang@etudiant.univ-rennes1.fr>

--- .config/nvim/lua/auto-dark-mode-cfg/init.lua
+++ .config/nvim/lua/auto-dark-mode-cfg/init.lua
@@ -1,39 +1,60 @@
 local auto_dark_mode = require "auto-dark-mode"
 
 Set_light_mode = function()
+	vim.api.nvim_create_autocmd(
+		"ColorScheme",
+		{
+			callback = function()
+				vim.cmd "highlight NormalFloat guibg=white"
+				vim.cmd "highlight FloatBorder guifg=black guibg=white"
+
+				vim.cmd "highlight DiagnosticError gui=bold"
+				vim.cmd "highlight DiagnosticWarn gui=italic"
+				vim.cmd "highlight DiagnosticInfo guibg=black guibg=italic"
+				-- vim.cmd "highlight DiagnosticHint guibg=black"
+
+				vim.cmd "highlight TreesitterContext guibg=#EEEEEE"
+				vim.cmd "highlight TreesitterContextBottom guibg=#EEEEEE gui=underline guisp=Grey"
+				print("light mode got called")
+			end,
+			group = vim.api.nvim_create_augroup("MyColorOverride", { clear = true }),
+			pattern = '*',
+		}
+	)
+
 	vim.o.background = "light"
-
-	vim.cmd "highlight NormalFloat guibg=white"
-	vim.cmd "highlight FloatBorder guifg=black guibg=white"
-
-	vim.cmd "highlight DiagnosticError gui=bold"
-	vim.cmd "highlight DiagnosticWarn gui=italic"
-	vim.cmd "highlight DiagnosticInfo guibg=black guibg=italic"
-	-- vim.cmd "highlight DiagnosticHint guibg=black"
-
-	vim.cmd "highlight TreesitterContext guibg=#EEEEEE"
-	vim.cmd "highlight TreesitterContextBottom guibg=#EEEEEE gui=underline guisp=Grey"
 end
 
 Set_dark_mode = function()
+	vim.api.nvim_create_autocmd(
+		"ColorScheme",
+		{
+			callback = function()
+				vim.cmd "highlight NormalFloat guibg=black"
+				vim.cmd "highlight FloatBorder guifg=white guibg=black"
+
+				vim.cmd "highlight DiagnosticError gui=bold"
+				vim.cmd "highlight DiagnosticWarn gui=italic"
+				-- vim.cmd "highlight DiagnosticInfo guifg=white guibg=black"
+				-- vim.cmd "highlight DiagnosticHint guifg=white guibg=black"
+
+				vim.cmd "highlight TreesitterContext guibg=#555555"
+				vim.cmd "highlight TreesitterContextBottom  guibg=#555555 gui=underline guisp=LightGrey"
+				print("dark mode got called")
+			end,
+			group = vim.api.nvim_create_augroup("MyColorOverride", { clear = true }),
+			pattern = '*',
+		}
+	)
+
 	vim.o.background = "dark"
-
-	vim.cmd "highlight NormalFloat guibg=black"
-	vim.cmd "highlight FloatBorder guifg=white guibg=black"
-
-	vim.cmd "highlight DiagnosticError gui=bold"
-	vim.cmd "highlight DiagnosticWarn gui=italic"
-	-- vim.cmd "highlight DiagnosticInfo guifg=white guibg=black"
-	-- vim.cmd "highlight DiagnosticHint guifg=white guibg=black"
-
-	vim.cmd "highlight TreesitterContext guibg=#555555"
-	vim.cmd "highlight TreesitterContextBottom  guibg=#555555 gui=underline guisp=LightGrey"
 end
 
 -- Use light by default
 Set_light_mode()
 
 auto_dark_mode.setup({
+	update_interval = 3000,
 	set_light_mode = Set_light_mode,
 	set_dark_mode = Set_dark_mode,
 })
