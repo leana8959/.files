9450e33f672eb567ff7711d79550a10da6608954
Léana 江 <leana.jiang@icloud.com>
2023-03-09 12:52:07 +0100

    cleanup(nvim)
    Signed-off-by: Chiang Léana <leana.chiang@etudiant.univ-rennes1.fr>

--- .config/nvim/lua/nvim-tree-cfg/init.lua
+++
@@ -1,8 +0,0 @@
-require("nvim-tree").setup({
-	view = {
-		number = true,
-		relativenumber = true,
-	}
-})
-
-vim.keymap.set("n", "<leader><space>", function() require('nvim-tree.api').tree.toggle() end)
