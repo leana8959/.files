3cd7ed6f6def36c5b0b48b0c516c66461574b3a7
Léana 江 <leana.jiang@icloud.com>
2023-03-22 19:04:37 +0100

    add(fish): largest object in repo fish function cleaned up other fish functions cleaned up nvim lsp-config

--- .config/fish/functions/brew_update.fish
+++ .config/fish/functions/brew_update.fish
@@ -1,6 +1,5 @@
 #!/opt/homebrew/bin/fish
 function brew_update
-    echo ===== (date) =====
     /opt/homebrew/bin/brew update
     /opt/homebrew/bin/brew upgrade
     /opt/homebrew/bin/brew autoremove
