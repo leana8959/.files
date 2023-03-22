46478ec5cfe2924584acf1e0c63988e2290b5d8a
Léana 江 <leana.jiang@icloud.com>
2023-03-22 21:55:38 +0100

    Revert "add(fish): alias cd='z'" zoxide calls cd, causes recursive loop

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -5,9 +5,7 @@ alias hist='history | nl | tac | less -S +G'
 alias v='nvim'
 
 alias gvim='neovide'
-alias cd='z'
 
-# OS-based aliases
 if test (uname) = "Linux"
     alias chmod='chmod --preserve-root'
     alias chown='chown --preserve-root'
@@ -15,4 +13,6 @@ else if test (uname) = "Darwin"
     alias hide_desktop='defaults write com.apple.finder CreateDesktop false; killall Finder'
     alias show_desktop='defaults write com.apple.finder CreateDesktop true; killall Finder'
     alias reset_launchpad='defaults write com.apple.dock ResetLaunchPad -bool true; killall Dock'
+else
+    # Neither, do nothing
 end
