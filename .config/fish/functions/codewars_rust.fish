c48ffc5c7d84c4a3a7305222215e612e6b278e55
Léana 江 <leana.jiang@icloud.com>
2023-03-26 01:14:30 +0100

    fix(fish): updated `codewars_rust` with new, simpler path convention

--- .config/fish/functions/codewars_rust.fish
+++ .config/fish/functions/codewars_rust.fish
@@ -1,19 +1,19 @@
 function codewars_rust
     if count $argv >/dev/null
-        set -f kata_name "codewars_"(snakecase $argv)
+        set -f kata_name (snakecase $argv)
         set -f kata_date (date +%Y.%-m.%-d)
-        mkdir -p ~/repos/codewars/Rust/"$argv"
-        mkdir -p ~/repos/codewars/Rust/"$argv"/src
-        touch ~/repos/codewars/Rust/"$argv"/src/main.rs
-        echo >~/repos/codewars/Rust/"$argv"/Cargo.toml "\
+        mkdir -p ~/repos/codewars/Rust/"$kata_name"
+        mkdir -p ~/repos/codewars/Rust/"$kata_name"/src
+        touch ~/repos/codewars/Rust/"$kata_name"/src/main.rs
+        echo >~/repos/codewars/Rust/"$kata_name"/Cargo.toml "\
 [package]
-name = \"$kata_name\"
+name = \"codewars_$kata_name\"
 version = \"$kata_date\"
 authors = [ \"Léana 江 <leana.jiang@icloud.com>\" ]
 
 [dependencies]
 "
-        cd ~/repos/codewars/Rust/"$argv"/ && nvim ./src/main.rs && cd
+        cd ~/repos/codewars/Rust/"$kata_name"/ && nvim ./src/main.rs && cd
     else
         echo "You must provide a new project name"
         return 1
