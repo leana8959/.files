dadc328e915e6bcd0d5a2006fb35a57b917e0753
Léana 江 <leana.jiang@icloud.com>
2023-03-28 21:10:38 +0200

    add(nvim): ISwap swap nodes with a keystroke

--- .config/nvim/lua/packer-cfg/init.lua
+++ .config/nvim/lua/packer-cfg/init.lua
@@ -69,6 +69,7 @@ require "packer".startup(function(use)
 	use { "hrsh7th/nvim-cmp",
 		requires = { "hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip" },
 	}
+	use "mizlan/iswap.nvim"
 
 
 	----> LSP and AST <----
