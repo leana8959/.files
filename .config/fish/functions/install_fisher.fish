b61cdb853cf49afc9e1dc1a196a0f2abe7fce988
Léana 江 <leana.jiang@icloud.com>
2023-03-30 21:14:43 +0200

    add(fish): sponge plugin: keep failed commands out of history

--- .config/fish/functions/install_fisher.fish
+++ .config/fish/functions/install_fisher.fish
@@ -1,6 +1,17 @@
 function install_fisher --description 'Install fisher and its plugins'
+    # fisher
     curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-    fisher install PatrickF1/fzf.fish # versatile fzf search https://github.com/PatrickF1/fzf.fish
-    fisher install patrickf1/colored_man_pages.fish # https://github.com/PatrickF1/colored_man_pages.fish
-    # fisher install laughedelic/pisces # matching paired symbols https://github.com/laughedelic/pisces
+
+    # versatile fzf search https://github.com/PatrickF1/fzf.fish
+    fisher install PatrickF1/fzf.fish
+
+    # https://github.com/PatrickF1/colored_man_pages.fish
+    fisher install patrickf1/colored_man_pages.fish
+
+    # matching paired symbols https://github.com/laughedelic/pisces
+    # fisher install laughedelic/pisces
+
+    # only keep command that succeeded in history
+    fisher install meaningful-ooo/sponge
+
 end
