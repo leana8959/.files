5b52d79cc4516e40bba56fc0a5f3701367cc84a1
Léana 江 <leana.jiang@icloud.com>
2023-03-31 14:43:27 +0200

    ref(fish): improved comments

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,25 +1,23 @@
 if status is-interactive
-    # Set editor
+    # Default editor
     set -gx EDITOR nvim
 
     # Set TTY for GPG
     set -gx GPG_TTY (tty)
 
-    # Set fzf layout & theme
+    # fzf layout & theme
     set -gx FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker=">"
     --color=fg:#4d4d4c,bg:#eeeeee,hl:#d7005f
     --color=fg+:#4d4d4c,bg+:#e8e8e8,hl+:#d7005f
     --color=info:#4271ae,prompt:#8959a8,pointer:#d7005f
     --color=marker:#4271ae,spinner:#4271ae,header:#4271ae'
+    # fzf preview theme (bat)
     set -gx fzf_preview_file_cmd "bat --style=numbers --color=always --theme OneHalfLight"
 
-    # Set fd theme
+    # fd theme
     set -gx LS_COLORS (vivid -m 24-bit generate one-light)
 
-    # Source starship
     starship init fish | source
-
-    # Source zoxide
     zoxide init fish --hook="prompt" | source
 end
 
