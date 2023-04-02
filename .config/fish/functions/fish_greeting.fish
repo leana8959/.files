e6f3f96a15a925daadb39fb7a93d32d444a5dc1d
Léana 江 <leana.jiang@icloud.com>
2023-04-02 17:01:13 +0200

    ref(starship): improved config legibility

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,4 +1,7 @@
 function fish_greeting
-    set_color $fish_color_option --bold --italics
-    cat ~/TOH-Quotes/* | shuf -n 1
+    switch $greeting
+        case "TOH"
+            set_color $fish_color_option --bold --italics
+            cat ~/TOH-Quotes/* | shuf -n 1
+    end
 end
