16c269d450a86ee0abe206f837505f739ebfbdbd
Léana 江 <leana.jiang@icloud.com>
2023-04-03 21:47:24 +0200

    add(fish): abbr simplification

--- .config/nvim/lua/glow-cfg/init.lua
+++ .config/nvim/lua/glow-cfg/init.lua
@@ -2,7 +2,7 @@ require "glow".setup {
   glow_path = "glow",            -- will be filled automatically with your glow bin in $PATH, if any
   install_path = "~/.local/bin", -- default path for installing glow binary
   border = "shadow",             -- floating window border config
-  style = "dark",                -- filled automatically with your current editor background, you can override using glow json style
+  style = "light",                -- filled automatically with your current editor background, you can override using glow json style
   pager = false,
   width = 80,
   height = 100,
