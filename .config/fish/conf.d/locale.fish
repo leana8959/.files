e552e45de232acf54e15c1a01abc6229c28d2c41
Léana 江 <leana.jiang@icloud.com>
2023-04-22 16:48:12 +0200

    switched locale to `zh_TW`

--- .config/fish/conf.d/locale.fish
+++ .config/fish/conf.d/locale.fish
@@ -1,7 +1,14 @@
-# set -gx LANG zh_TW.UTF-8
-# set -gx LANGUAGE zh_TW
-# set -gx LC_ALL zh_TW.UTF-8
+# zh_TW
+set -gx LANG zh_TW.UTF-8
+set -gx LANGUAGE zh_TW
+set -gx LC_ALL zh_TW.UTF-8
 
-set -gx LANG fr_FR.UTF-8
-set -gx LANGUAGE fr_FR
-set -gx LC_ALL fr_FR.UTF-8
+# fr_FR
+# set -gx LANG fr_FR.UTF-8
+# set -gx LANGUAGE fr_FR
+# set -gx LC_ALL fr_FR.UTF-8
+
+# en_US
+# set -gx LANG en_US.UTF-8
+# set -gx LANGUAGE en_US
+# set -gx LC_ALL en_US.UTF-8
