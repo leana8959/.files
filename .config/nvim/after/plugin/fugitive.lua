f9251f542f76313fc1a7783f439e1e2f0ca3fe4a
Léana 江 <leana.jiang@icloud.com>
2023-04-28 23:42:28 +0200

    add(nvim): fugitive toggle

--- .config/nvim/after/plugin/fugitive.lua
+++ .config/nvim/after/plugin/fugitive.lua
@@ -1,1 +1,11 @@
-vim.keymap.set('n', "<leader>G", vim.cmd.Git, { desc = "open fugitive" })
+vim.keymap.set('n', "<leader>g",
+	":Git<CR><Down><Down><Down><Down><Down>",
+	{ desc = "open fugitive" }
+)
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "fugitive" },
+	callback = function()
+		vim.keymap.set("n", "<leader>g", ":q<CR>", { desc = "close fugitive", buffer = true })
+	end,
+})
--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -22,7 +22,7 @@ vim.keymap.set('n', '<leader>/',
 	{ desc = '[/] Fuzzily search in current buffer]' }
 )
 vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Search [A]ll [F]iles' })
-vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Search [G]it [F]iles' })
+-- vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Search [G]it [F]iles' })
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
