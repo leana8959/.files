9c99a6dec0f880b60e91e8069832c5a96a1912bd
Léana 江 <leana.jiang@icloud.com>
2023-04-29 22:45:09 +0200

    fix(fish): tmux_attach variable names were messed up lol

--- .config/fish/functions/tmux_attach.fish
+++ .config/fish/functions/tmux_attach.fish
@@ -2,9 +2,10 @@ function tmux_attach --description "attach to existing tmux sessions"
     set -f selected (tmux list-sessions -F "#{session_name}" | fzf)
 
     if test -z "$TMUX"
-        tmux attach-session -t "$selected_name"
+        tmux attach-session -t "$selected"
+        return 0
     else
-        tmux switch-client -t "$selected_name"
+        tmux switch-client -t "$selected"
     end
 
 end
