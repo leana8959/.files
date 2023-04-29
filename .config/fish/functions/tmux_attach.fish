21284b4795176b9d0f7fce8e8f97d2fde32c8389
Léana 江 <leana.jiang@icloud.com>
2023-04-29 22:47:23 +0200

    fix(fish): oopsie, missed something

--- .config/fish/functions/tmux_attach.fish
+++ .config/fish/functions/tmux_attach.fish
@@ -1,6 +1,10 @@
 function tmux_attach --description "attach to existing tmux sessions"
     set -f selected (tmux list-sessions -F "#{session_name}" | fzf)
 
+    if test -z $selected
+        return 0
+    end
+
     if test -z "$TMUX"
         tmux attach-session -t "$selected"
         return 0
