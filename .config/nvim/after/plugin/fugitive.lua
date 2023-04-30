c161a88190d50a8eaa66a0d39feea93431b93416
Léana 江 <leana.jiang@icloud.com>
2023-04-30 02:31:35 +0200

    add(nvim): fugitive blame maps

--- .config/nvim/after/plugin/fugitive.lua
+++ .config/nvim/after/plugin/fugitive.lua
@@ -1,11 +1,15 @@
-vim.keymap.set('n', "<leader>g",
-	":Git<CR>5<Down>",
-	{ desc = "open fugitive" }
-)
-
+vim.keymap.set('n', "<leader>G", ":Git<CR>5<Down>", { desc = "open fugitive" })
 vim.api.nvim_create_autocmd({ "FileType" }, {
 	pattern = { "fugitive" },
 	callback = function()
-		vim.keymap.set("n", "<leader>g", ":q<CR>", { desc = "close fugitive", buffer = true })
+		vim.keymap.set("n", "<leader>G", ":q<CR>", { desc = "close fugitive", buffer = true })
+	end,
+})
+
+vim.keymap.set('n', "<leader>gb", ":Git blame<CR>", { desc = "open fugitive blame" })
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "fugitiveblame" },
+	callback = function()
+		vim.keymap.set("n", "<leader>gb", ":q<CR>", { desc = "close fugitive blame", buffer = true })
 	end,
 })
