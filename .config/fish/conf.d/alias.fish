d867e484ac5da667276da08bd638edcc755ae68e
Léana 江 <leana.jiang@icloud.com>
2023-05-01 21:34:23 +0200

    fix(nvim): cc would collide with [c] [c]ompiler :/

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -20,7 +20,7 @@ abbr gs 'git switch'
 abbr gp 'git push'
 abbr gpr 'git fetch && git rebase'
 abbr gpm 'git fetch && git merge'
-abbr cc 'clone_repo'
+abbr clone 'clone_repo'
 
 ## FS
 alias tree='tree -Cph -L 1'
