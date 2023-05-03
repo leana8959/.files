ae0d3f562edfa1e63d6031ec1f113268774e8b7d
Léana 江 <leana.jiang@icloud.com>
2023-05-03 22:02:51 +0200

    ref(fish): tmux_attach

--- .config/fish/functions/tmux_attach.fish
+++ .config/fish/functions/tmux_attach.fish
@@ -1,13 +1,12 @@
 function tmux_attach --description "attach to existing tmux sessions"
     set -f selected (tmux list-sessions -F "#{session_name}" | fzf)
 
-    if test -z $selected
+    if [ -z $selected ]
         return 0
     end
 
-    if test -z "$TMUX"
+    if [ -z "$TMUX" ]
         tmux attach-session -t "$selected"
-        return 0
     else
         tmux switch-client -t "$selected"
     end
