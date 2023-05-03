90dcaa6742d5b0d50722a9346d3d13bddab94fe6
Léana 江 <leana.jiang@icloud.com>
2023-05-03 22:00:10 +0200

    feat(fish): open new playground right from the sessionizer :fire:

--- .config/fish/functions/tmux_sessionizer.fish
+++ .config/fish/functions/tmux_sessionizer.fish
@@ -1,26 +1,40 @@
 # Inspired by the one and only primeagen
 
 function tmux_sessionizer --description "create tmux sessions"
-    set selected (find ~/repos ~/univ-repos -mindepth 2 -maxdepth 2 -type d | fzf)
+    set selected \
+        (begin
+            find ~/repos ~/univ-repos -mindepth 2 -maxdepth 2 -type d;
+            find ~/playground -mindepth 1 -maxdepth 1 -type d;
+            echo "play"
+        end | fzf)
 
     if test -z $selected
         return 0
     end
 
-    set selected_name (echo $selected | tr . _)
+    if [ "$selected" = "play" ]
+        read -P "Name a new playground" selected
+        if test -z $selected
+            return 0
+        else
+            set selected ~/playground/"$selected"
+            mkdir -p $selected
+        end
+    end
 
+    set selected_name (echo $selected | tr . _)
     set tmux_running (pgrep tmux)
 
-    if test -z "$TMUX" && test -z "$tmux_running"
+    if [ -z "$TMUX" ] && [ -z "$tmux_running" ]
         tmux new-session -s "$selected_name" -c "$selected"
         return 0
     end
 
-    if not tmux has-session -t="$selected_name" 2> /dev/null
+    if ! tmux has-session -t="$selected_name" 2> /dev/null
         tmux new-session -ds "$selected_name" -c "$selected"
     end
 
-    if test -z "$TMUX"
+    if [ -z "$TMUX" ]
         tmux attach-session -t "$selected_name"
     else
         tmux switch-client -t "$selected_name"
