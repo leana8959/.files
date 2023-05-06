3e4c65a2688e13e05627904ada81d3d2219d9460
Léana 江 <leana.jiang@icloud.com>
2023-05-06 20:40:17 +0200

    ref(fish): simplification

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,7 +1,4 @@
 function fish_greeting
-    switch $greeting
-        case "TOH"
-            set_color $fish_color_option --bold --italics
-            cat ~/TOH-Quotes/* | shuf -n 1
-    end
+    set_color $fish_color_option --bold --italics
+    cat ~/TOH-Quotes/* | shuf -n 1
 end
