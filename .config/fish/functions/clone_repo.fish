9e3ae9752e6afbaa8629bb09bdb4a792768a2642
Léana 江 <leana.jiang@icloud.com>
2023-05-06 18:39:43 +0200

    mod(fish): removed `cd` in clone_repo

--- .config/fish/functions/clone_repo.fish
+++ .config/fish/functions/clone_repo.fish
@@ -3,9 +3,8 @@ function clone_repo
         set name_repo (echo $argv | sd -p '.*[:/]([\w\d._-]+)/([\w\d._-]+).git$' '$1 $2')
         set name (echo $name_repo | cut -d ' ' -f1)
         set repo (echo $name_repo | cut -d ' ' -f2)
-        mkdir -p ~/repos/"$name"
-        git clone $argv ~/repos/"$name"/"$repo"
-        cd ~/repos/"$name"/"$repo"
+        mkdir -p $REPOS_PATH/$name
+        git clone $argv $REPOS_PATH/$name/$repo
     else
         echo "Please provide a git url"
     end
