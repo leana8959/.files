b722ec17e9c22f762b4c3d27a61e541aced58d80
Léana 江 <leana.jiang@icloud.com>
2023-05-12 22:08:58 +0200

    add(fish): new modes for online judge platforms

--- .config/fish/functions/tmux_sessionizer.fish
+++ .config/fish/functions/tmux_sessionizer.fish
@@ -5,10 +5,14 @@ function tmux_sessionizer --description "create tmux sessions"
         (begin
             fd . $REPOS_PATH $UNIV_REPOS_PATH --exact-depth 2 --type d;
             fd . $PLAYGROUND_PATH --exact-depth 1 --type d;
-            fd . $CODEWARS_PATH/rust --exact-depth 1 --type d;
+            fd . $CODEWARS_PATH/Rust --exact-depth 1 --type d;
+            fd . $CODEWARS_PATH/Haskell --exact-depth 1 --type d;
+            fd . $CODEWARS_PATH/C --exact-depth 1 --type d;
             fd . $ZEROJUDGE_PATH --exact-depth 1 --type d;
             echo "play";
             echo "codewars rust";
+            echo "codewars haskell";
+            echo "codewars c";
             echo "zerojudge c";
         end | fzf)
 
@@ -33,6 +37,28 @@ function tmux_sessionizer --description "create tmux sessions"
             set kata_name (snakecase $name)
             set selected $CODEWARS_PATH/Rust/$kata_name
             cargo new $selected --name codewars_$kata_name --vcs none
+        end
+
+    case "codewars haskell"
+        read -P "Give it a name: " name
+        if test -z $name
+            return 0
+        else
+            set name (snakecase $name)
+            set selected $CODEWARS_PATH/Haskell/$name
+            mkdir -p $selected
+            touch $selected/main.hs
+        end
+
+    case "codewars c"
+        read -P "Give it a name: " name
+        if test -z $name
+            return 0
+        else
+            set name (snakecase $name)
+            set selected $CODEWARS_PATH/C/$name
+            mkdir -p $selected
+            touch $selected/main.c
         end
 
     case "zerojudge c"
