29839f96e5982c72f0bc8fcf6a5c54ea41e8d4a3
Léana 江 <leana.jiang@icloud.com>
2023-06-09 18:56:55 +0200

    mod(nvim): keymaps inspired my helix

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -21,8 +21,8 @@ vim.keymap.set('n', '<leader>/',
 	function() builtin.current_buffer_fuzzy_find(themes.get_dropdown { previewer = false }) end,
 	{ desc = '[/] Fuzzily search in current buffer]' }
 )
-vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Search [A]ll [F]iles' })
--- vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Search [G]it [F]iles' })
+vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Search [A]ll [F]iles' })
+vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Search [G]it [F]iles' })
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
