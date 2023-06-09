1c97b1b33aa319e83beaeb7c60417fe925896dc6
Léana 江 <leana.jiang@icloud.com>
2023-06-09 18:56:55 +0200

    mod(nvim): keymaps inspired my helix

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -30,7 +30,7 @@ local on_attach = function(_, bufnr)
 	vim.keymap.set('n', 'gu', ts.lsp_references, { desc = "LSP Usages (Telescope)", buffer = bufnr })
 	vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = "Code action", buffer = bufnr })
 	vim.keymap.set('n', '<leader>cl', vim.lsp.codelens.run, { desc = "Code lens", buffer = bufnr })
-	vim.keymap.set('n', '<leader>f',
+	vim.keymap.set('n', '<leader>=',
 		function() vim.lsp.buf.format { async = true } end,
 		{ desc = "LSP format", buffer = bufnr }
 	)
