54212f8eaaf7338473cda56383badfc409c9c097
Léana 江 <leana.jiang@icloud.com>
2023-06-11 13:29:08 +0200

    mod(nvim): reverted helix inspired keymaps

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -21,10 +21,10 @@ vim.keymap.set('n', '<leader>/',
 	function() builtin.current_buffer_fuzzy_find(themes.get_dropdown { previewer = false }) end,
 	{ desc = '[/] Fuzzily search in current buffer]' }
 )
-vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Search [A]ll [F]iles' })
+vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Search [A]ll [F]iles' })
 vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Search [G]it [F]iles' })
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'Search [B]uffers' })
+vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch [B]uffers' })
