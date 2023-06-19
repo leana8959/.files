92663fac27b142681cb310b1511ec02d2e51de11
Léana 江 <leana.jiang@icloud.com>
2023-06-19 09:02:38 +0200

    oopsie

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,9 +1,9 @@
--- require("fidget").setup({
--- 	text = {
--- 		spinner = "dots",
--- 		-- done = "[Ok]",
--- 	},
--- })
+require("fidget").setup({
+	text = {
+		spinner = "dots",
+		-- done = "[Ok]",
+	},
+})
 
 require("nvim-treesitter.configs").setup({
 	-- A list of parser names, or "all" (the four listed parsers should always be installed)
