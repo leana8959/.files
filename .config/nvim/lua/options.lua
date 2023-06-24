627f21d9af3961afbf487d9999295c41ba6e5a9c
Léana 江 <leana.jiang@icloud.com>
2023-06-24 23:42:06 +0200

    mod(nvim): use hard tabs by default

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -7,9 +7,7 @@ vim.opt.signcolumn = "yes"
 vim.opt.filetype = "on"
 
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.expandtab = false
 
 -- wrapping makes the editor EXTREMELY slow, turn it off by default
 vim.opt.wrap = false
