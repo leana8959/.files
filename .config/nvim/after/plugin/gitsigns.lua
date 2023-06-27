c4d6bc0c5a869abf8beaf32c3497518bceef32b8
Léana 江 <leana.jiang@icloud.com>
2023-06-27 12:16:37 +0200

    add(nvim): gitsign bindings

--- .config/nvim/after/plugin/gitsigns.lua
+++ .config/nvim/after/plugin/gitsigns.lua
@@ -7,7 +7,7 @@ require "gitsigns".setup({
     changedelete = { text = '~' },
     untracked    = { text = '·' },
   },
-  signcolumn                   = true,  -- Toggle with `:Gitsigns toggle_signs`
+  signcolumn                   = false, -- Toggle with `:Gitsigns toggle_signs`
   numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
   linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
   word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
@@ -40,3 +40,13 @@ require "gitsigns".setup({
     enable = false
   },
 })
+
+vim.keymap.set("n",
+  "<leader>gl",
+  function()
+    vim.cmd("Gitsigns toggle_numhl")
+    vim.cmd("Gitsigns toggle_signs")
+    vim.cmd("Gitsigns toggle_word_diff")
+  end,
+  { desc = "[G]it High[l]ight" }
+)
