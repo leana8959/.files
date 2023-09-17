2d02342cca19c18038d6e560c05bcd67cdd15ad7
Léana 江 <leana.jiang@icloud.com>
2023-09-17 21:29:30 +0200

    add(nvim): metals + dap configured This time I actually (kinda) know what I'm doing

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -63,9 +63,10 @@ function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
     return orig_util_open_floating_preview(contents, syntax, opts, ...)
 end
 
--- LSPs
+-- LSPs / DAPs
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require "cmp_nvim_lsp".default_capabilities(capabilities)
+
 -- Spell check
 local common_dictionary = {
     -- Me stuff
@@ -96,31 +97,37 @@ require "lspconfig".ltex.setup {
     flags = { debounce_text_changes = 10000 },
     capabilities = capabilities,
 }
+
 -- JSON
 require "lspconfig".jsonls.setup {
     on_attach = on_attach,
     capabilities = capabilities,
 }
+
 -- XML
 require "lspconfig".lemminx.setup {
     on_attach = on_attach,
     capabilities = capabilities,
 }
+
 -- CSS
 require "lspconfig".cssls.setup {
     on_attach = on_attach,
     capabilities = capabilities,
 }
+
 -- Lua
 require "lspconfig".lua_ls.setup {
     on_attach = on_attach,
     capabilities = capabilities,
 }
+
 -- Go
 require "lspconfig".gopls.setup {
     on_attach = on_attach,
     capabilities = capabilities,
 }
+
 -- Rust
 require "rust-tools".setup {
     server = {
@@ -128,37 +135,75 @@ require "rust-tools".setup {
         cmd = { "rustup", "run", "stable", "rust-analyzer" }
     }
 }
+
 -- tex
 require "lspconfig".texlab.setup {
     on_attach = on_attach,
     capabilities = capabilities,
 }
+
 -- Markdown
 require "lspconfig".marksman.setup {
     on_attach = on_attach,
     capabilities = capabilities,
 }
+
 -- TOML
 require "lspconfig".taplo.setup {
     on_attach = on_attach,
     capabilities = capabilities,
 }
+
 -- Java
 require "lspconfig".jdtls.setup {
     on_attach = on_attach,
     capabilities = capabilities,
 }
+
 -- Scala
 local metals = require "metals"
 local metals_config = metals.bare_config()
 metals_config.capabilities = capabilities
-metals_config.on_attach = on_attach
+
+require "dap".configurations.scala = {
+    {
+        type = "scala",
+        request = "launch",
+        name = "RunOrTest",
+        metals = { runType = "runOrTestFile", },
+    },
+    {
+        type = "scala",
+        request = "launch",
+        name = "Test Target",
+        metals = { runType = "testTarget", },
+    },
+}
+
+metals_config.on_attach = function(client, bufnr)
+    metals.setup_dap()
+
+    map("n", "<leader>ws", metals.hover_worksheet)
+
+    map("n", "<leader>dc", require "dap".continue)
+    map("n", "<leader>dr", require("dap").repl.toggle)
+    map("n", "<leader>dK", require("dap.ui.widgets").hover)
+    map("n", "<leader>dt", require("dap").toggle_breakpoint)
+    map("n", "<leader>dso", require("dap").step_over)
+    map("n", "<leader>dsi", require("dap").step_into)
+    map("n", "<leader>dl", require("dap").run_last)
+
+    on_attach(client, bufnr)
+end
+local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
     pattern = { "scala", "sbt", "java" },
     callback = function()
         require "metals".initialize_or_attach(metals_config)
-    end
+    end,
+    group = nvim_metals_group
 })
+
 -- Haskell
 vim.g.haskell_tools = {
     tools = {
@@ -186,33 +231,39 @@ vim.g.haskell_tools = {
         }
     }
 }
+
 -- Python
 require "lspconfig".pylsp.setup {
     on_attach = on_attach,
     capabilities = capabilities,
 }
+
 -- C
 require "lspconfig".clangd.setup {
     on_attach = on_attach,
     capabilities = capabilities,
 }
+
 -- HTML
 require "lspconfig".html.setup {
     on_attach = on_attach,
     capabilities = capabilities,
     filetypes = { "html", "xhtml" },
 }
+
 -- Bash
 require "lspconfig".bashls.setup {
     on_attach = on_attach,
     capabilities = capabilities,
     filetypes = { "sh" },
 }
+
 -- TypeScript
 require "lspconfig".tsserver.setup {
     on_attach = on_attach,
     capabilities = capabilities,
 }
+
 -- Typst
 require "lspconfig".typst_lsp.setup {
     on_attach = on_attach,
@@ -221,4 +272,5 @@ require "lspconfig".typst_lsp.setup {
         exportPdf = "onType"
     }
 }
+
 vim.filetype.add({ extension = { typ = "typst" } })
