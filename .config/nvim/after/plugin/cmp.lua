dd43ecf5471b4c92474e3605124b5b075b903cfb
Léana 江 <leana.jiang@icloud.com>
2023-09-29 17:27:42 +0200

    ref: unified to spaces

--- .config/nvim/after/plugin/cmp.lua
+++ .config/nvim/after/plugin/cmp.lua
@@ -4,56 +4,56 @@ local luasnip = require "luasnip"
 require "luasnip.loaders.from_vscode".lazy_load({ paths = { "./snippets" } })
 
 luasnip.config.set_config({
-	region_check_events = 'InsertEnter',
-	delete_check_events = 'InsertLeave'
+    region_check_events = 'InsertEnter',
+    delete_check_events = 'InsertLeave'
 })
 
 local has_words_before = function()
-	unpack = unpack or table.unpack
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body)
-		end,
-	},
-	mapping = cmp.mapping.preset.insert {
-		['<C-d>'] = cmp.mapping.scroll_docs(-4),
-		['<C-u>'] = cmp.mapping.scroll_docs(4),
-		-- ['<C-Space>'] = cmp.mapping.complete(),
-		['<CR>'] = cmp.mapping.confirm {
-			behavior = cmp.ConfirmBehavior.Insert,
-			select = true,
-		},
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-				-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-				-- they way you will only jump inside the snippet region
-			elseif luasnip.expand_or_locally_jumpable() then
-				luasnip.expand_or_jump()
-			elseif has_words_before() then
-				cmp.complete()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-	},
-	sources = {
-		{ name = "buffer" },
-		{ name = "nvim_lsp" },
-		{ name = "luasnip" },
-	},
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert {
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-u>'] = cmp.mapping.scroll_docs(4),
+        -- ['<C-Space>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true,
+        },
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+                -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+                -- they way you will only jump inside the snippet region
+            elseif luasnip.expand_or_locally_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+    },
+    sources = {
+        { name = "buffer" },
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+    },
 })
--- .config/nvim/after/plugin/fugitive.lua
+++ .config/nvim/after/plugin/fugitive.lua
@@ -1,15 +1,15 @@
 vim.keymap.set('n', "<leader><space>", ":Git<CR>5<Down>", { desc = "open fugitive" })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-	pattern = { "fugitive" },
-	callback = function()
-		vim.keymap.set("n", "<leader><space>", ":q<CR>", { desc = "close fugitive", buffer = true })
-	end,
+    pattern = { "fugitive" },
+    callback = function()
+        vim.keymap.set("n", "<leader><space>", ":q<CR>", { desc = "close fugitive", buffer = true })
+    end,
 })
 
 vim.keymap.set('n', "<leader>gb", ":Git blame<CR>", { desc = "open fugitive blame" })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-	pattern = { "fugitiveblame" },
-	callback = function()
-		vim.keymap.set("n", "<leader>gb", ":q<CR>", { desc = "close fugitive blame", buffer = true })
-	end,
+    pattern = { "fugitiveblame" },
+    callback = function()
+        vim.keymap.set("n", "<leader>gb", ":q<CR>", { desc = "close fugitive blame", buffer = true })
+    end,
 })
