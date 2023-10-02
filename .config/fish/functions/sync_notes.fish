179e4481dbf5ed51670c41d7181d7b693d900e7a
Léana 江 <leana.jiang@icloud.com>
2023-10-02 06:50:13 +0200

    ref(fish): cleaned up functions

--- .config/fish/functions/sync_notes.fish
+++
@@ -1,5 +0,0 @@
-function sync_notes
-    rsync --recursive --verbose --chown=caddy:caddy --chmod=u=rwX,g=rX,o=rX --exclude='.DS_Store' --update --times --compress --delay-updates --delete \
-        '/Users/leana/Documents/Rennes 1/L2/Prise de notes/' \
-        'earth2077.fr:/srv/http/notes/'
-end
