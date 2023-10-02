3a736784084e19d71f54f14a9f543bfdb574473e
Léana 江 <leana.jiang@icloud.com>
2023-10-02 21:13:42 +0200

    fix(fish): `java_home` setting is now platform dependent

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -35,12 +35,13 @@ if status is-interactive
     set -gx fish_cursor_replace_one underscore
     set -gx fish_cursor_visual block
 
-    # Java Home
-    set -x JAVA_HOME (/usr/libexec/java_home -v 17)
-    alias java $JAVA_HOME/bin/java
-
-    # SBT JVM setting
-    alias sbt "sbt -java-home $JAVA_HOME"
+    if [ (uname) = "Darwin" ]
+        # Java Home
+        set -x JAVA_HOME (/usr/libexec/java_home -v 17)
+        alias java $JAVA_HOME/bin/java
+        # SBT JVM setting
+        alias sbt "sbt -java-home $JAVA_HOME"
+    end
 
     # Ansible path
     set -gx ANSIBLE_CONFIG ~/.ansible.cfg
