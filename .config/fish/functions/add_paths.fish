ad60f321134141970aee79050c14be63a00d5ec0
Léana 江 <leana.jiang@icloud.com>
2023-10-02 06:50:13 +0200

    ref(fish): cleaned up functions

--- .config/fish/functions/add_paths.fish
+++ .config/fish/functions/add_paths.fish
@@ -1,4 +1,4 @@
-function add_paths
+function add_paths --description "add to PATH idempotently"
     contains -- $argv $fish_user_paths
     or set --universal fish_user_paths $fish_user_paths $argv
     echo "Updated PATH: $PATH"
