bd3a7c9b57f9536a324d09b9fd92fac3c0cb1756
Léana 江 <leana.jiang@icloud.com>
2023-10-08 01:31:26 +0200

    add(nvim): lua typst snippet yay

--- .config/nvim/after/plugin/cmp.lua
+++ .config/nvim/after/plugin/cmp.lua
@@ -1,29 +1,62 @@
 local cmp = require "cmp"
-local luasnip = require "luasnip"
+local ls = require "luasnip"
 
-require "luasnip.loaders.from_vscode".lazy_load({ paths = { "./snippets" } })
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local l = require("luasnip.extras").lambda
+local rep = require("luasnip.extras").rep
+local p = require("luasnip.extras").partial
+local m = require("luasnip.extras").match
+local n = require("luasnip.extras").nonempty
+local dl = require("luasnip.extras").dynamic_lambda
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local types = require("luasnip.util.types")
+local conds = require("luasnip.extras.conditions")
+local conds_expand = require("luasnip.extras.conditions.expand")
 
-luasnip.config.set_config({
+-- require "luasnip.loaders.from_vscode".lazy_load({ paths = { "./snippets" } })
+
+-- Some hack
+ls.config.set_config({
     region_check_events = 'InsertEnter',
     delete_check_events = 'InsertLeave'
 })
-
 local has_words_before = function()
     unpack = unpack or table.unpack
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
+ls.add_snippets("typst", {
+    s("entry", {
+        t({
+            "#entry(",
+            "  " .. os.date("(year: %Y, month: %m, day:%d)"),
+            ")[",
+            "",
+        }),
+        i(0),
+        t({ "", "]" })
+    }),
+})
+
 cmp.setup({
     snippet = {
         expand = function(args)
-            luasnip.lsp_expand(args.body)
+            ls.lsp_expand(args.body)
         end,
     },
     mapping = cmp.mapping.preset.insert {
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-u>'] = cmp.mapping.scroll_docs(4),
-        -- ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-Space>'] = cmp.mapping.complete(),
         ['<CR>'] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Insert,
             select = true,
@@ -33,8 +66,8 @@ cmp.setup({
                 cmp.select_next_item()
                 -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
                 -- they way you will only jump inside the snippet region
-            elseif luasnip.expand_or_locally_jumpable() then
-                luasnip.expand_or_jump()
+            elseif ls.expand_or_locally_jumpable() then
+                ls.expand_or_jump()
             elseif has_words_before() then
                 cmp.complete()
             else
@@ -44,16 +77,16 @@ cmp.setup({
         ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
+            elseif ls.jumpable(-1) then
+                ls.jump(-1)
             else
                 fallback()
             end
         end, { "i", "s" }),
     },
     sources = {
-        { name = "buffer" },
-        { name = "nvim_lsp" },
         { name = "luasnip" },
+        { name = "nvim_lsp" },
+        { name = "buffer" },
     },
 })
