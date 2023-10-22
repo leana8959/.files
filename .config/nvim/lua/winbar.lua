173068bdbc495930b8a22bce7aa31612da5fdee1
Léana 江 <leana.jiang@icloud.com>
2023-10-22 12:37:49 +0200

    add(nvim): winbar

---
+++ .config/nvim/lua/winbar.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+vim.api.nvim_set_hl(0, 'WinBarPath', { bg = '#dedede', })
+vim.api.nvim_set_hl(0, 'WinBarModified', { bg = '#dedede', bold = true })
+
+function M.eval()
+    local file_path = vim.api.nvim_eval_statusline('%f', {}).str
+    local modified = vim.api.nvim_eval_statusline('%M', {}).str == '+' and '[+]' or ''
+
+    return ""
+        .. '%#WinBarModified#'
+        .. modified
+        .. '%*'
+        .. '%#WinBarPath#'
+        .. " "
+        .. file_path
+        -- .. '%*'
+end
+
+return M
