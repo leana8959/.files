4f9c81132a76672f434de3321e386b531e842b07
Léana 江 <leana.jiang@icloud.com>
2023-10-22 13:05:59 +0200

    add(nvim): right align winbar

--- .config/nvim/lua/winbar.lua
+++ .config/nvim/lua/winbar.lua
@@ -7,7 +7,7 @@ function M.eval()
     local file_path = vim.api.nvim_eval_statusline('%f', {}).str
     local modified = vim.api.nvim_eval_statusline('%M', {}).str == '+' and '[+]' or ''
 
-    return ""
+    return "%="
         .. '%#WinBarModified#'
         .. modified
         .. '%*'
