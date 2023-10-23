012cc5431542da08935958f07a4ecab8e9bd7d97
Léana 江 <leana.jiang@icloud.com>
2023-10-23 20:42:06 +0200

    add(nvim): integration of autopair with cmp

--- .config/nvim/after/plugin/autopairs.lua
+++ .config/nvim/after/plugin/autopairs.lua
@@ -1,1 +1,6 @@
 require "nvim-autopairs".setup()
+
+-- Intergration with `cmp`
+local cmp = require "cmp"
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
