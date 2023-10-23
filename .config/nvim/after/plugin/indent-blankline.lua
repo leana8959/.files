1d6235f01d42829df2ad9fe94e179ae43705014a
Léana 江 <leana.jiang@icloud.com>
2023-10-23 20:34:26 +0200

    ref(nvim): huge cleanup

--- .config/nvim/after/plugin/indent-blankline.lua
+++ .config/nvim/after/plugin/indent-blankline.lua
@@ -1,3 +1,12 @@
 require "ibl".setup {
-    indent = { char = "", },
+    indent = {
+        char = "",
+        smart_indent_cap = true,
+    },
+    scope = {
+        char = "▎",
+        enabled = true,
+        show_start = false,
+        show_end = false,
+    }
 }
