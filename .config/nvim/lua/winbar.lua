c3d81627124faae440ca2250f9612b91948d662b
Léana 江 <leana.jiang@icloud.com>
2023-10-23 10:10:48 +0200

    fix(nvim): leading space of winbar

--- .config/nvim/lua/winbar.lua
+++ .config/nvim/lua/winbar.lua
@@ -5,16 +5,15 @@ vim.api.nvim_set_hl(0, 'WinBarModified', { bg = '#dedede', bold = true })
 
 function M.eval()
     local file_path = vim.api.nvim_eval_statusline('%f', {}).str
-    local modified = vim.api.nvim_eval_statusline('%M', {}).str == '+' and '[+]' or ''
+    local modified = vim.api.nvim_eval_statusline('%M', {}).str == '+' and '[+] ' or ''
 
     return "%="
         .. '%#WinBarModified#'
         .. modified
         .. '%*'
         .. '%#WinBarPath#'
-        .. " "
         .. file_path
-        -- .. '%*'
+        .. '%*'
 end
 
 return M
