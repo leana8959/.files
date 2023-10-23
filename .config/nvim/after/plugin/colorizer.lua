ac590decc27df81e4e6b95de9441dad9bd452076
Léana 江 <leana.jiang@icloud.com>
2023-10-23 21:11:35 +0200

    fix(nvim): colorizer setup

--- .config/nvim/after/plugin/colorizer.lua
+++ .config/nvim/after/plugin/colorizer.lua
@@ -1,19 +1,21 @@
-local prefs =
-{
-    RGB      = true,
-    RRGGBB   = true,
-    names    = false,
-    RRGGBBAA = true,
-    rgb_fn   = true,
-    hsl_fn   = true,
-    css      = true,
-    css_fn   = true,
+local default = {
+    RGB           = true,  -- #RGB hex codes
+    RRGGBB        = true,  -- #RRGGBB hex codes
+    names         = false, -- "Name" codes like Blue or blue
+    RRGGBBAA      = false, -- #RRGGBBAA hex codes
+    AARRGGBB      = false, -- 0xAARRGGBB hex codes
+    rgb_fn        = false, -- CSS rgb() and rgba() functions
+    hsl_fn        = false, -- CSS hsl() and hsla() functions
+    css           = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+    css_fn        = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+
+    mode          = "background",
+    always_update = true,
 }
 
-require "colorizer".setup {
-    lua = prefs,
-    css = prefs,
-    scss = prefs,
-    fish = prefs,
-    toml = prefs,
-}
+require "colorizer".setup({
+    filetypes = {
+        '*',
+    },
+    user_default_options = default,
+})
