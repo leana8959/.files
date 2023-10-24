f13d82e9dbcd95450552164c53e53e2e0480933e
Léana 江 <leana.jiang@icloud.com>
2023-10-24 23:49:08 +0200

    ref(nvim): harpoon

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -1,18 +1,19 @@
 require "harpoon".setup()
 
-local ui = require "harpoon.ui"
+local ui   = require "harpoon.ui"
 local mark = require "harpoon.mark"
+local map  = vim.keymap.set
 
 -- add and view
-vim.keymap.set({ 'n', 'i' }, '<A-c>', function() mark.add_file() end, { desc = "add file to harpoon" })
-vim.keymap.set({ 'n', 'i' }, '<A-g>', function() ui.toggle_quick_menu() end, { desc = "show harpoon menu" })
+map({ 'n', 'i' }, '<A-c>', function() mark.add_file() end)
+map({ 'n', 'i' }, '<A-g>', function() ui.toggle_quick_menu() end)
 
 -- switch it up!
-vim.keymap.set({ 'n', 'i' }, '<A-h>', function() ui.nav_file(1) end, { desc = "harpoon 1 (the magic finger)" })
-vim.keymap.set({ 'n', 'i' }, '<A-t>', function() ui.nav_file(2) end, { desc = "harpoon 2" })
-vim.keymap.set({ 'n', 'i' }, '<A-n>', function() ui.nav_file(3) end, { desc = "harpoon 3" })
-vim.keymap.set({ 'n', 'i' }, '<A-s>', function() ui.nav_file(4) end, { desc = "harpoon 4" })
-vim.keymap.set({ 'n', 'i' }, '<A-m>', function() ui.nav_file(5) end, { desc = "harpoon 5" })
-vim.keymap.set({ 'n', 'i' }, '<A-w>', function() ui.nav_file(6) end, { desc = "harpoon 6" })
-vim.keymap.set({ 'n', 'i' }, '<A-v>', function() ui.nav_file(7) end, { desc = "harpoon 7" })
-vim.keymap.set({ 'n', 'i' }, '<A-z>', function() ui.nav_file(8) end, { desc = "harpoon 8" })
+map({ 'n', 'i' }, '<A-h>', function() ui.nav_file(1) end)
+map({ 'n', 'i' }, '<A-t>', function() ui.nav_file(2) end)
+map({ 'n', 'i' }, '<A-n>', function() ui.nav_file(3) end)
+map({ 'n', 'i' }, '<A-s>', function() ui.nav_file(4) end)
+map({ 'n', 'i' }, '<A-m>', function() ui.nav_file(5) end)
+map({ 'n', 'i' }, '<A-w>', function() ui.nav_file(6) end)
+map({ 'n', 'i' }, '<A-v>', function() ui.nav_file(7) end)
+map({ 'n', 'i' }, '<A-z>', function() ui.nav_file(8) end)
