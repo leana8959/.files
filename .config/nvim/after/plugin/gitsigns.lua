855d9ec25fd1d85b19640310bcc58cfd7fcc8f2e
Léana 江 <leana.jiang@icloud.com>
2023-10-24 23:44:00 +0200

    ref(nvim): gitsigns

--- .config/nvim/after/plugin/gitsigns.lua
+++ .config/nvim/after/plugin/gitsigns.lua
@@ -24,28 +24,4 @@ require "gitsigns".setup({
     ignore_whitespace = false,
   },
   current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
-  sign_priority                = 6,
-  update_debounce              = 100,
-  status_formatter             = nil,   -- Use default
-  max_file_length              = 40000, -- Disable if file is longer than this (in lines)
-  preview_config               = {
-    -- Options passed to nvim_open_win
-    border = 'single',
-    style = 'minimal',
-    relative = 'cursor',
-    row = 0,
-    col = 1
-  },
-  yadm                         = {
-    enable = false
-  },
 })
-
-vim.keymap.set("n",
-  "<leader>gl",
-  function()
-    vim.cmd("Gitsigns toggle_numhl")
-    vim.cmd("Gitsigns toggle_signs")
-  end,
-  { desc = "[G]it High[l]ight" }
-)
