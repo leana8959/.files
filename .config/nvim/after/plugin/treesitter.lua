46140359bb9d41bd2894b4660efced4109ca5349
Léana 江 <leana.jiang@icloud.com>
2023-10-25 00:06:48 +0200

    ref(nvim): treesitter

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,8 +1,8 @@
 require("nvim-treesitter.configs").setup({
     ensure_installed = {},
-    sync_install = false,
-    auto_install = true,
-    highlight = {
+    sync_install     = false,
+    auto_install     = true,
+    highlight        = {
         enable = true,
         additional_vim_regex_highlighting = false,
     },
