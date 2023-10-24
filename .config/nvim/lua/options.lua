b26dfd7874a73c0d4dee2af6d241e9436a92ef54
Léana 江 <leana.jiang@icloud.com>
2023-10-24 23:26:53 +0200

    ref(nvim): isolated autocmds

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,6 +1,4 @@
 local opt          = vim.opt
-local api          = vim.api
-local map          = vim.keymap.set
 
 opt.hlsearch       = false
 opt.incsearch      = true
@@ -14,8 +12,7 @@ opt.tabstop        = 4
 opt.expandtab      = true
 opt.shiftwidth     = 4
 
--- wrapping makes the editor EXTREMELY slow, turn it off by default
-opt.wrap           = false
+opt.wrap           = false -- Slows the editor
 opt.linebreak      = true
 opt.breakindent    = true
 opt.filetype       = "on"
@@ -41,60 +38,3 @@ opt.foldlevelstart = 99
 opt.foldenable     = true
 
 opt.winbar         = "%{%v:lua.require'winbar'.eval()%}"
-
-api.nvim_create_autocmd("TextYankPost", {
-    pattern = "*",
-    callback = function() vim.highlight.on_yank() end,
-})
-
-opt.listchars = {
-    tab = " ",
-    trail = "␣",
-}
-opt.list = true
-
-api.nvim_create_autocmd("FileType", {
-    pattern = { "markdown", "tex", "typst" },
-    callback = function()
-        vim.opt_local.shiftwidth = 4
-        vim.opt_local.tabstop = 4
-        vim.cmd("setlocal wrap")
-    end,
-})
-
-api.nvim_create_autocmd("FileType", {
-    pattern = { "rust" },
-    callback = function() vim.cmd("setlocal iskeyword+=&") end,
-})
-
-api.nvim_create_autocmd("FileType", {
-    pattern = { "fish" },
-    callback = function() vim.cmd("setlocal iskeyword+=$") end,
-})
-
--- Using `sudoedit` would create gibberish extension names,
--- detection using extension would hence not work.
-api.nvim_create_autocmd("BufEnter", {
-    pattern  = { "*Caddyfile*" },
-    callback = function()
-        vim.opt_local.filetype = "Caddy"
-        vim.bo.commentstring   = "# %s"
-    end,
-})
-
-api.nvim_create_autocmd("Filetype", {
-    pattern  = { "skel" },
-    callback = function()
-        vim.bo.commentstring = "(* %s *)"
-        map('n', '<leader>f',
-            function()
-                vim.cmd(":w")
-                vim.cmd([[silent exec "!necroprint % -o %"]])
-                vim.cmd(":e")
-            end,
-            { buffer = true })
-    end,
-})
-
-vim.filetype.add({ extension = { typ = "typst" } })
-vim.filetype.add({ extension = { skel = "skel", sk = "skel" } })
