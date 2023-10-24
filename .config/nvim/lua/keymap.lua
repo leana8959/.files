c22e1ba2205034266645636d2e92d9deb97eb4e1
Léana 江 <leana.jiang@icloud.com>
2023-10-24 23:18:52 +0200

    ref(nvim): cleaned up keymap

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -3,49 +3,48 @@ vim.g.maplocalleader = " "
 
 local map = vim.keymap.set
 
--- From THE one and only "Primeagen"
-map("v", "J", ":m '>+1<CR>gv=gv")
-map("v", "K", ":m '<-2<CR>gv=gv")
 
-map("n", "<C-d>", "<C-d>zz")
+map("v", "J", ":m '>+1<CR>gv=gv") -- Move up
+map("v", "K", ":m '<-2<CR>gv=gv") -- Move down
+
+map("n", "<C-d>", "<C-d>zz")      -- Centered motions
 map("n", "<C-u>", "<C-u>zz")
 map("n", "n", "nzzzv")
 map("n", "N", "Nzzzv")
-
-map("n", "<leader>pv", function() vim.cmd("Explore") end)
-
 map("n", "J", "mzJ`z")
 
-map({ "n", "x", "v" }, "<leader>d", '"_d')
+map({ "n", "x", "v" }, "<leader>d", '"_d') -- Better clipboard
 map({ "n", "x", "v" }, "<leader>c", '"_dc')
 map({ "n", "x", "v" }, "<leader>p", '"_dP')
 map({ "n", "x", "v" }, "<leader>y", '"+y')
 
-map("n", "Q", "<nop>")
-
-map("n", "<leader>nf", function() vim.cmd("enew") end)
-map("n", "<leader>so", function() vim.cmd("so %") end)
-
-map("c", "#lm", [[\{-}]])
-map("c", "#capl", [[\(.\{-}\)]])
-map("c", "#capm", [[\(.*\)]])
-
-map("n", "j", "gj")
+map("n", "j", "gj") -- linewrap jk
 map("n", "k", "gk")
 map("n", "<Down>", "g<Down>")
 map("n", "<Up>", "g<Up>")
 
-map("n", "<leader>gP", function() vim.cmd("G push") end)
+map("n", "<leader>pv", function() vim.cmd("Explore") end) -- Project View
+
+map("n", "<leader>nf", function() vim.cmd("enew") end)    -- New File
+map("n", "<leader>so", function() vim.cmd("so %") end)    -- Source buffer
+
+map("c", "#capl", [[\(.\{-}\)]])                          -- helpers in regex
+map("c", "#capm", [[\(.*\)]])
+
+map("n", "<leader>gP", function() vim.cmd("G push") end) -- Git
 map("n", "<leader>gp", function() vim.cmd("G pull") end)
 
-map("n", "<leader>w", function() vim.cmd.setlocal("invwrap") end)
-map("n", "<leader>hg", function() vim.cmd("Inspect") end)
+map("n", "<leader>+x", function() vim.cmd("!chmod +x %") end) -- Permission
+map("n", "<leader>-x", function() vim.cmd("!chmod -x %") end)
 
-map("n", "<leader>ta", function() vim.cmd("Tabularize /=") end)
+map("n", "<leader>w", function() vim.cmd.setlocal("invwrap") end) -- linewrap
+
+map("n", "<leader>ta", function() vim.cmd("Tabularize /=") end)   -- Alignment
 map("n", "<leader>tc", function() vim.cmd("Tabularize /:") end)
 map("n", "<leader>tC", function() vim.cmd("Tabularize trailing_c_comments") end)
 
-map("n", "<leader>+x", function() vim.cmd("!chmod +x %") end)
-map("n", "<leader>-x", function() vim.cmd("!chmod -x %") end)
+map("n", "<leader>hg", function() vim.cmd("Inspect") end) -- Highlight Group
 
 map("n", "<leader>tw", function() vim.cmd("Twilight") end)
+
+map("n", "Q", "<nop>") -- *do not* repeat the last recorded register [count] times.
