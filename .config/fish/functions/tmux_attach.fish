30d7dd415db9890407b03b98463cd1a25706216b
Léana 江 <leana.jiang@icloud.com>
2023-10-29 11:47:33 +0100

    fix(fish): tmux_attach

--- .config/fish/functions/tmux_attach.fish
+++ .config/fish/functions/tmux_attach.fish
@@ -1,10 +1,17 @@
 function tmux_attach --description "attach to existing tmux sessions"
-    set selected (tmux list-sessions -F "#{session_name}" | fzf)
 
+    # abandon if tmux not running
+    set tmux_running (pgrep tmux)
+    if [ -z "$tmux_running" ]
+        return 0
+    end
+
+    set selected (tmux list-sessions -F "#{session_name}" | fzf)
     if [ -z $selected ]
         return 0
     end
 
+    # attach or switch
     if [ -z "$TMUX" ]
         tmux attach-session -t $selected
     else
