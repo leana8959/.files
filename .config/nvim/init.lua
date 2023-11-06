babebbe05cad47ebb9514fde89a8c094bcdb93eb
Léana 江 <leana.jiang@icloud.com>
2023-11-06 19:41:30 +0100

    add(nvim): TS playground

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -135,6 +135,7 @@ require "lazy".setup {
     ----------------
     { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
     "nvim-treesitter/nvim-treesitter-context",
+    "nvim-treesitter/playground",
 
     -----------
     -- Games --
