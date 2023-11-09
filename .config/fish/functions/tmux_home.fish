3825839535a6e7bf5b39db708a52e5aa9cc34b96
Léana 江 <leana.jiang@icloud.com>
2023-11-09 20:32:16 +0100

    ref(fish): tmux_home

--- .config/fish/functions/tmux_home.fish
+++ .config/fish/functions/tmux_home.fish
@@ -9,23 +9,20 @@ function tmux_home
             new-session -s $session_name \; \
             new-window -t $session_name \; \
             select-window -t $session_name:1 \;
-        return 0
-    end
-
-
-    # create session is doesn't exist
-    if ! tmux has-session -t=$session_name 2> /dev/null
-        tmux \
-            new-session -ds $session_name \; \
-            new-window -t $session_name \; \
-            select-window -t $session_name:1 \;
-    end
-
-    # attach or switch
-    if [ -z $TMUX ]
-        tmux attach-session -t $session_name
     else
-        tmux switch-client -t $session_name
+        # create session is doesn't exist
+        if ! tmux has-session -t=$session_name 2> /dev/null
+            tmux \
+                new-session -ds $session_name \; \
+                new-window -t $session_name \; \
+                select-window -t $session_name:1 \;
+        end
+        # attach or switch
+        if [ -z $TMUX ]
+            tmux attach-session -t $session_name
+        else
+            tmux switch-client -t $session_name
+        end
     end
 
 end
