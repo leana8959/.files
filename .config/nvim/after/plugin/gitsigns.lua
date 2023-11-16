8a0bbe20bd37f53544a60db8bd28d28285c7fa7f
Léana 江 <leana.jiang@icloud.com>
2023-11-16 13:58:35 +0100

    fix(nvim): keybinds

--- .config/nvim/after/plugin/gitsigns.lua
+++ .config/nvim/after/plugin/gitsigns.lua
@@ -9,26 +9,26 @@ require "gitsigns".setup {
         end
 
         -- Navigation
-        map("n", "hj", gs.next_hunk)
-        map("n", "hk", gs.prev_hunk)
+        map("n", "<leader>hj", gs.next_hunk)
+        map("n", "<leader>hk", gs.prev_hunk)
 
         -- Actions
-        map("n", "hs", gs.stage_hunk)
-        map("n", "hu", gs.undo_stage_hunk)
-        map("n", "hr", gs.reset_hunk)
+        map("n", "<leader>hs", gs.stage_hunk)
+        map("n", "<leader>hu", gs.undo_stage_hunk)
+        map("n", "<leader>hr", gs.reset_hunk)
 
-        map("v", "hs", function() gs.stage_hunk { vim.fn.line ".", vim.fn.line "v" } end)
-        map("v", "hr", function() gs.reset_hunk { vim.fn.line ".", vim.fn.line "v" } end)
+        map("v", "<leader>hs", function() gs.stage_hunk { vim.fn.line ".", vim.fn.line "v" } end)
+        map("v", "<leader>hr", function() gs.reset_hunk { vim.fn.line ".", vim.fn.line "v" } end)
 
-        map("n", "hS", gs.stage_buffer)
-        map("n", "hR", gs.reset_buffer)
+        map("n", "<leader>hS", gs.stage_buffer)
+        map("n", "<leader>hR", gs.reset_buffer)
 
-        map("n", "hp", gs.preview_hunk)
-        map("n", "hb", function() gs.blame_line { full = true } end)
-        map("n", "tb", gs.toggle_current_line_blame)
+        map("n", "<leader>hp", gs.preview_hunk)
+        map("n", "<leader>hb", function() gs.blame_line { full = true } end)
+        map("n", "<leader>tb", gs.toggle_current_line_blame)
 
-        map("n", "hd", gs.diffthis)
-        map("n", "hD", function() gs.diffthis "~" end)
+        map("n", "<leader>hd", gs.diffthis)
+        map("n", "<leader>hD", function() gs.diffthis "~" end)
 
         map("n", "<leader>td", gs.toggle_deleted)
 
