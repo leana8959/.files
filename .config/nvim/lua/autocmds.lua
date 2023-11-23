6525337f629a3ce00ae2c6c35e23df2bf3ceb8e8
Léana 江 <leana.jiang@icloud.com>
2023-11-23 08:58:03 +0100

    add(nvim): Retab!

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,5 +1,6 @@
 local map = vim.keymap.set
 local autocmd = vim.api.nvim_create_autocmd
+local usercmd = vim.api.nvim_create_user_command
 
 vim.filetype.add { extension = { typ = "typst" } }
 vim.filetype.add { extension = { skel = "skel", sk = "skel" } }
@@ -48,3 +49,14 @@ autocmd("OptionSet", {
 
 -- Enable NoNeckPain
 autocmd("UIEnter", { callback = function() vim.cmd "NoNeckPain" end })
+
+-- Retab file with specified shiftwidth
+usercmd("Retab", function(opts)
+    local dst         = tonumber(opts.fargs[1])
+    vim.opt.expandtab = false
+    vim.cmd "%retab!"
+    vim.opt.shiftwidth = dst
+    vim.opt.tabstop    = dst
+    vim.opt.expandtab  = true
+    vim.cmd "%retab!"
+end, { nargs = 1 })
