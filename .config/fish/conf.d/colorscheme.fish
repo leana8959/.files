87b716a3c9270c10e1b346982474d1845e097445
Léana 江 <leana.jiang@icloud.com>
2023-11-25 09:55:54 +0100

    fish: updated colorscheme

--- .config/fish/conf.d/colorscheme.fish
+++ .config/fish/conf.d/colorscheme.fish
@@ -1,6 +1,7 @@
 # Learn more: https://fishshell.com/docs/current/interactive.html
 
-set black 000000
+set black 383A42
+set white ffffff
 set grey a0a1a7
 set cyan 0184bc
 set blue 4078f2
@@ -11,66 +12,85 @@ set red ca1243
 set brown 986801
 set gold c18401
 set accent 645199
-set tinted_bg eeeeee
+set visual d0d0d0
 
-## General
-# Default color
-set -gx fish_color_normal $black
-# Commands (binary)
-set -gx fish_color_command $blue
-# Options starting with “-”, up to the first “--” parameter
-set -gx fish_color_param $red
-# Regular command parameters
-set -gx fish_color_option $red
-# Quoted blocks of text
-set -gx fish_color_quote $green
-# IO redirections
-set -gx fish_color_redirection $gold
-# Process separators like ';' and '&'
-set -gx fish_color_end $black --bold
-# The color used to highlight potential errors (invalid command)
-set -gx fish_color_error $red
-# Code comments
-set -gx fish_color_comment $grey
-# Highlight matching parenthesis
-set -gx fish_color_match --background=$red
-# Used in `dirh` https://fishshell.com/docs/current/cmds/dirh.html
-set -gx fish_color_history_current --italics --bold
-# Parameter expansion operators like `*` and `~`
-set -gx fish_color_operator $blue
-# Character escapes like \n
-set -gx fish_color_escape $purple
-# Parameters that are filenames (if the file exists)
-set -gx fish_color_valid_path --underline --bold
-# Autosuggestion
-set -gx fish_color_autosuggestion $grey
-# The `^C` indicator on a canceled command
-set -gx fish_color_cancel --reverse $accent
-# History search matches and selected pager items (background only)
-# Default value is black (background)
-set -gx fish_color_search_match --reverse $accent
-
-## Prompt (useless with starship)
+###########
+# General #
+###########
+# default color
+set -g fish_color_normal $black
+# commands like echo
+set -g fish_color_command $blue
+# keywords like if - this falls back on the command color if unset
+set -g fish_color_keyword $purple
+# quoted text like "abc"
+set -g fish_color_quote $green
+# IO redirections like >/dev/null
+set -g fish_color_redirection $gold
+# process separators like ; and &
+set -g fish_color_end $black --bold
+# syntax errors
+set -g fish_color_error $grey --italics
+# ordinary command parameters
+set -g fish_color_param $red
+# parameters that are filenames (if the file exists)
+set -g fish_color_valid_path --italics
+# options starting with “-”, up to the first “--” parameter
+set -g fish_color_option $red
+# comments like ‘# important’
+set -g fish_color_comment $grey
+# selected text in vi visual mode
+set -g fish_color_selection --background=$visual
+# parameter expansion operators like * and ~
+set -g fish_color_operator $orange
+# character escapes like \n and \x70
+set -g fish_color_escape $purple
+# autosuggestions (the proposed rest of a command)
+set -g fish_color_autosuggestion $accent
 # The current working directory in the default prompt
-set -gx fish_color_cwd $black
+set -g fish_color_cwd $black
 # The current working directory in the default prompt for the root user
-set -gx fish_color_cwd_root $black
+set -g fish_color_cwd_root $red
 # The username in the default prompt
-set -gx fish_color_user $black
+set -g fish_color_user $grey
 # The hostname in the default prompt
-set -gx fish_color_host $black
+set -g fish_color_host $black
+# the hostname in the default prompt for remote sessions (like ssh)
+set -g fish_color_host_remote $red
+# the last command’s nonzero exit code in the default prompt
+set -g fish_color_status $red
+# the ‘^C’ indicator on a canceled command
+set -g fish_color_cancel $accent --reverse
+# history search matches and selected pager items (background only)
+set -g fish_color_search_match --reverse
 
-## Vi
-set -gx fish_color_selection --background=$tinted_bg
-
-## Pager
-# The progress bar at the bottom left corner
-set -gx fish_pager_color_progress --reverse $cyan
-# The prefix string, i.e. the string that is to be completed
-set -gx fish_pager_color_prefix --bold --underline
-# The completion itself, i.e. the proposed rest of the string
-set -gx fish_pager_color_completion $black
-# The completion description
-set -gx fish_pager_color_description $gold --italics #caramel
-# Background of the selected completion
-set -gx fish_pager_color_selected_background --reverse
+#########
+# Pager #
+#########
+# the progress bar at the bottom left corner
+set -g fish_pager_color_progress --reverse $cyan
+# the background color of a line
+set -g fish_pager_color_background $black
+# the prefix string, i.e. the string that is to be completed
+set -g fish_pager_color_prefix $accent
+# the completion itself, i.e. the proposed rest of the string
+set -g fish_pager_color_completion $black
+# the completion description
+set -g fish_pager_color_description $grey
+# background of the selected completion
+set -g fish_pager_color_selected_background --reverse
+# prefix of the selected completion
+set -g fish_pager_color_selected_prefix $red
+# suffix of the selected completion
+set -g fish_pager_color_selected_completion
+# description of the selected completion
+set -g fish_pager_color_selected_description $gold
+# ## Alternating colors
+# # background of every second unselected completion
+# set -g fish_pager_color_secondary_background --background=$tinted_bg
+# # prefix of every second unselected completion
+# set -g fish_pager_color_secondary_prefix
+# # suffix of every second unselected completion
+# set -g fish_pager_color_secondary_completion
+# # description of every second unselected completion
+# set -g fish_pager_color_secondary_description
