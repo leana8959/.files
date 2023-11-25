498a8eb23bd8ad00deb774dd52c842ea3a269f6e
Léana 江 <leana.jiang@icloud.com>
2023-11-25 15:57:14 +0100

    fix: fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,41 +3,41 @@ if status is-interactive
     set -gx EDITOR nvim
 
     # Set TTY for GPG
-    set -gx GPG_TTY (tty)
+    set -g GPG_TTY (tty)
 
     # fzf layout & theme
-    set -gx FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker=">"
+    set -g FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker=">"
     --color=fg:#4d4d4c,bg:#eeeeee,hl:#d7005f
     --color=fg+:#4d4d4c,bg+:#e8e8e8,hl+:#d7005f
     --color=info:#4271ae,prompt:#8959a8,pointer:#d7005f
     --color=marker:#4271ae,spinner:#4271ae,header:#4271ae'
 
     # fzf preview theme (bat)
-    set -gx fzf_preview_file_cmd 'delta'
+    set -g fzf_preview_file_cmd 'delta'
 
     # fd theme
-    set -gx LS_COLORS (vivid -m 24-bit generate one-light)
+    set -g LS_COLORS (vivid -m 24-bit generate one-light)
 
     # repo paths
     # universal because tmux_sessionizer is run in non-interactive mode
-    set -Ux REPOS_PATH ~/repos
-    set -Ux UNIV_REPOS_PATH ~/univ-repos
-    set -Ux PLAYGROUND_PATH ~/playground
-    set -Ux CODEWARS_PATH ~/codewars
-    set -Ux ZEROJUDGE_PATH ~/zerojudge
+    set -U REPOS_PATH ~/repos
+    set -U UNIV_REPOS_PATH ~/univ-repos
+    set -U PLAYGROUND_PATH ~/playground
+    set -U CODEWARS_PATH ~/codewars
+    set -U ZEROJUDGE_PATH ~/zerojudge
 
     # delta
-    set -gx DELTA_FEATURES +side-by-side
+    set -g DELTA_FEATURES +side-by-side
 
     # gopath
-    set -gx GOPATH ~/.go
+    set -g GOPATH ~/.go
 
     # vi cursor style
     fish_vi_key_bindings
-    set -gx fish_cursor_default block
-    set -gx fish_cursor_insert line
-    set -gx fish_cursor_replace_one underscore
-    set -gx fish_cursor_visual block
+    set -g fish_cursor_default block
+    set -g fish_cursor_insert line
+    set -g fish_cursor_replace_one underscore
+    set -g fish_cursor_visual block
 
     # OCaml opam environment
     if command -q opam
@@ -45,7 +45,7 @@ if status is-interactive
     end
 
     # Ansible path
-    set -gx ANSIBLE_CONFIG ~/.ansible.cfg
+    set -g ANSIBLE_CONFIG ~/.ansible.cfg
 
     # tools
     starship init fish | source
