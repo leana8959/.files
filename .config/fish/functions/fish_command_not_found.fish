14c04a177cc155f313ee77d6af0620d18726dc11
Léana 江 <leana.jiang@icloud.com>
2023-11-29 19:52:00 +0100

    add(fish): fish command not found gentle insults

---
+++ .config/fish/functions/fish_command_not_found.fish
@@ -0,0 +1,17 @@
+function fish_command_not_found
+
+    set hour (date +"%H hour")
+    set user (whoami)
+
+    set words                              \
+            "Are you drunk ??"             \
+            "Il est seulement $hour heure" \
+            "Reveille-toi, $user"          \
+            "Magique de vim !"             \
+            "As-tu ecris ton journal aujourd'hui ?"
+
+    set choice $words[(random 1 (count $words))]
+
+    echo -e $choice | figlet -c
+
+end
