0fb5cada89e4fd57c1f03532beb56f64c29497d7
Léana 江 <leana.jiang@icloud.com>
2023-11-29 19:50:07 +0100

    updated search script

--- .local/bin/search.py
+++ .local/bin/search.py
@@ -3,10 +3,14 @@
 import argparse
 import os
 
-SEARX = "https://searxng.earth2077.fr/search?q={query}"
-INVIDIOUS = "https://invidious.earth2077.fr/search?q={query}"
-GITHUB = "https://github.com/search?q={query}&type=repositories"
-FORGEJO = "https://git.earth2077.fr/?repo-search-query={query}"
+SEARX = "https://searxng.earth2077.fr/search?q=%s"
+INVIDIOUS = "https://invidious.earth2077.fr/search?q=%s"
+GITHUB = "https://github.com/search?q=%s&type=repositories"
+FORGEJO = "https://git.earth2077.fr/explore/repos?sort=recentupdate&language=&q=%s&only_show_relevant=false"
+WIKIPEDIA = "https://wikipedia.org/w/index.php?search=%s"
+YANDEX = "https://yandex.com/search?text=%s"
+GENIUS = "https://genius.com/search?q=%s"
+HOOGLE = "https://hoogle.haskell.org/?hoogle=%s"
 
 
 parser = argparse.ArgumentParser(description="Search the web, simply")
@@ -23,15 +27,24 @@ else:
     match args.motor[0]:
         case "yt":
             motor = INVIDIOUS
-        case "fj":
+        case "fg":
             motor = FORGEJO
         case "gh":
             motor = GITHUB
+        case "wk":
+            motor = WIKIPEDIA
+        case "yd":
+            motor = YANDEX
+        case "ge":
+            motor = GENIUS
+        case "hg":
+            motor = HOOGLE
         case _:
             motor = SEARX
 
 query = " ".join(args.query).replace(" ", "%20")
+link = motor[:].replace("%s", query)
 
-
-link = motor[:].replace("{query}", query)
+# make the system go
+print(link)
 os.system(f"open {link}")
