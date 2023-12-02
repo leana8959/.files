98df1067403846881223c964bcf5d7cfed9f81f3
Léana 江 <leana.jiang@icloud.com>
2023-12-02 19:47:11 +0100

    fix(fish): fish_remove_path operates now on `$fish_user_path`

--- .config/fish/functions/fish_remove_path.fish
+++ .config/fish/functions/fish_remove_path.fish
@@ -1,8 +1,8 @@
 function fish_remove_path
-    if set index (contains -i $argv[1] $PATH)
+    if set index (contains -i $argv[1] $fish_user_path)
         set --erase --universal fish_user_paths[$index]
-        echo "Updated PATH: $PATH"
+        echo "Updated fish_user_path: $fish_user_path"
     else
-        echo "$argv[1] not found in PATH: $PATH"
+        echo "$argv[1] not found in fish_user_path: $fish_user_path"
     end
 end
