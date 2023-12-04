78862992e0fc6de0df8a7404a2f70a302beb97d8
Léana 江 <leana.jiang@icloud.com>
2023-12-04 19:05:39 +0100

    add(nvim): keymap to leave terminal mode

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -62,6 +62,8 @@ map("n", "<leader>e", vim.diagnostic.open_float)
 map("n", "<leader>pe", vim.diagnostic.goto_prev)
 map("n", "<leader>ne", vim.diagnostic.goto_next)
 
+map("t", "<Leader><ESC>", "<C-\\><C-n>")
+
 -------------
 -- Plugins --
 -------------
