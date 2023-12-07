785c67fca4fc8970f8985c0c18d6d0923eb96933
Léana 江 <leana.jiang@icloud.com>
2023-12-07 16:19:32 +0100

    add(nvim): better winbar

--- .config/nvim/lua/winbar.lua
+++ .config/nvim/lua/winbar.lua
@@ -1,11 +1,15 @@
 local M = {}
 
-vim.api.nvim_set_hl(0, "WinBarPath", { bg = "#dedede" })
-vim.api.nvim_set_hl(0, "WinBarModified", { bg = "#dedede", bold = true })
+local api = vim.api
+local fn = vim.fn
+
+api.nvim_set_hl(0, "WinBar", { fg = "#dedede" })
+api.nvim_set_hl(0, "WinBarPath", { bg = "#dedede" })
+api.nvim_set_hl(0, "WinBarModified", { bg = "#dedede", bold = true })
 
 function M.eval()
-    local buffer = vim.api.nvim_win_get_buf(0)
-    local bufname = vim.fn.bufname(buffer)
+    local buffer = api.nvim_win_get_buf(0)
+    local bufname = fn.bufname(buffer)
     local modified = ""
     local readonly = ""
 
@@ -16,6 +20,8 @@ function M.eval()
     if vim.bo[buffer].modified then
         modified = "[+] "
     end
+
+    bufname = fn.fnamemodify(bufname, ":p:~")
 
     return "%="
         .. "%#WinBarModified#"
