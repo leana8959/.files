805992074f702c6478d06d6460a5b77288acd40f
Léana 江 <leana.jiang@icloud.com>
2023-12-07 15:55:03 +0100

    perf(nvim): more efficient winbar

--- .config/nvim/lua/winbar.lua
+++ .config/nvim/lua/winbar.lua
@@ -4,15 +4,26 @@ vim.api.nvim_set_hl(0, "WinBarPath", { bg = "#dedede" })
 vim.api.nvim_set_hl(0, "WinBarModified", { bg = "#dedede", bold = true })
 
 function M.eval()
-    local file_path = vim.api.nvim_eval_statusline("%f", {}).str
-    local modified = vim.api.nvim_eval_statusline("%M", {}).str == "+" and "[+] " or ""
+    local buffer = vim.api.nvim_win_get_buf(0)
+    local bufname = vim.fn.bufname(buffer)
+    local modified = ""
+    local readonly = ""
+
+    if vim.bo[buffer].readonly then
+        readonly = "[RO] "
+    end
+
+    if vim.bo[buffer].modified then
+        modified = "[+] "
+    end
 
     return "%="
         .. "%#WinBarModified#"
+        .. readonly
         .. modified
         .. "%*"
         .. "%#WinBarPath#"
-        .. file_path
+        .. bufname
         .. "%*"
 end
 
