b785b548cbed1d8168a7ee216b152ce50977a3b9
Léana 江 <leana.jiang@icloud.com>
2023-12-13 10:16:36 +0100

    fix(fish): fzf pure black

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -26,8 +26,8 @@ if status is-interactive
     --cycle
     --border=none
     --preview-window=wrap
-    --color=fg:#383A42,bg:#eeeeee,hl:#ca1243
-    --color=fg+:#383A42,bg+:#d0d0d0,hl+:#ca1243
+    --color=fg:#000000,bg:#eeeeee,hl:#ca1243
+    --color=fg+:#000000,bg+:#d0d0d0,hl+:#ca1243
     --color=info:#0184bc,prompt:#645199,pointer:#645199
     --color=marker:#0184bc,spinner:#645199,header:#645199
     --color=gutter:#eeeeee'
