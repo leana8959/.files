a59e258f5d9a053baa7f6c88181c8b2a2df4ae81
Léana 江 <leana.jiang@icloud.com>
2023-12-18 22:56:05 +0100

    nix: alejandra

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -1,110 +1,110 @@
-{ config, pkgs, ... }:
-
-let
+{
+  config,
+  pkgs,
+  ...
+}: let
   inherit (pkgs.stdenv.hostPlatform) isDarwin isLinux;
   inherit (pkgs.lib) optionals;
 
-  unstable = import (fetchTarball "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz") { };
-in
-{
+  unstable =
+    import
+    (fetchTarball "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz")
+    {};
+in {
   home.username = "leana";
-  home.homeDirectory = if (isDarwin) then "/Users/leana" else "/home/leana";
+  home.homeDirectory =
+    if isDarwin
+    then "/Users/leana"
+    else "/home/leana";
 
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
   nixpkgs.overlays = [
     (final: prev: {
       cmus = prev.cmus.overrideAttrs (old: {
-        patches = (old.patches or []) ++ [
-          (prev.fetchpatch {
-            url  = "https://github.com/cmus/cmus/commit/4123b54bad3d8874205aad7f1885191c8e93343c.patch";
-            hash = "sha256-YKqroibgMZFxWQnbmLIHSHR5sMJduyEv6swnKZQ33Fg=";
-          })
-        ];
+        patches =
+          (old.patches or [])
+          ++ [
+            (prev.fetchpatch {
+              url = "https://github.com/cmus/cmus/commit/4123b54bad3d8874205aad7f1885191c8e93343c.patch";
+              hash = "sha256-YKqroibgMZFxWQnbmLIHSHR5sMJduyEv6swnKZQ33Fg=";
+            })
+          ];
       });
     })
   ];
 
   home.packages = with pkgs; ([
+      # text/editors
+      helix
+      gnused
+      neovim
+      ripgrep
+      vim
+      tmux
 
-    # text/editors
-    helix
-    gnused
-    neovim
-    nil
-    ripgrep
-    vim
-    tmux
+      # nix
+      nil
+      alejandra
 
-    # shell
-    fish
-    (python39.withPackages(ps: with ps; [
-      beautifulsoup4
-      requests
-    ]))
-    stow
+      # shell
+      fish
+      (python39.withPackages (ps: with ps; [beautifulsoup4 requests]))
+      stow
 
-    fd
-    fzf
-    htop
-    starship
-    tree
-    vivid
-    rsync
+      fd
+      fzf
+      htop
+      starship
+      tree
+      vivid
+      rsync
 
-    # fancy utilities
-    figlet
-    macchina
-    ncdu
-    tldr
+      # fancy utilities
+      figlet
+      macchina
+      ncdu
+      tldr
 
-    # git related
-    bat
-    delta
-    gnupg
+      # git related
+      bat
+      delta
+      gnupg
+    ]
+    ++ optionals isDarwin [
+      (nerdfonts.override {
+        fonts = ["CascadiaCode" "JetBrainsMono" "Meslo"];
+      })
 
-  ] ++ optionals (isDarwin) [
+      asciinema
+      cmus
+      cmusfm
+      hyperfine
+      tea
+      yt-dlp
+      watchexec
 
-    (nerdfonts.override {
-      fonts = [
-        "CascadiaCode"
-        "JetBrainsMono"
-        "Meslo"
-      ];
-    })
+      jdk17
+      rustup
+      nodejs_20
 
-    asciinema
-    cmus
-    cmusfm
-    hyperfine
-    tea
-    yt-dlp
-    watchexec
+      # # NOTE: should be fixed soon
+      # unstable.opam
 
-    jdk17
-    rustup
-    nodejs_20
-
-    # # NOTE: should be fixed soon
-    # unstable.opam
-
-    unstable.typst
-
-  ] ++ optionals (isLinux) [
-
-    # # NOTE: doesn't work
-    # valgrind
-    gdb
-
-  ]);
+      unstable.typst
+    ]
+    ++ optionals isLinux [
+      # # NOTE: doesn't work
+      # valgrind
+      gdb
+    ]);
 
   programs = {
     home-manager.enable = true;
 
     direnv = {
-      enable            = true;
+      enable = true;
       nix-direnv.enable = true;
     };
   };
-
 }
