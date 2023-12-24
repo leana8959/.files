2443652900f8bcf900147fa6b95753ae087a2526
Léana 江 <leana.jiang@icloud.com>
2023-12-24 14:47:52 +0100

    ref: nixos config (removed home.nix)

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,39 +2,28 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixunstable.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
     nixpkgs,
     nixunstable,
-    home-manager,
     ...
   }: let
     inherit (nixpkgs) lib;
+    withSystem = (
+      system: hostname: (lib.nixosSystem {
+        specialArgs = {
+          inherit nixpkgs;
+          inherit nixunstable;
+          inherit system;
+          inherit hostname;
+        };
+        modules = [./hosts/thinkpad/configuration.nix];
+      })
+    );
   in {
     nixosConfigurations = {
-      thinkpad = let
-        system = "aarch64-linux";
-        unstable = import nixunstable {inherit system;};
-      in
-        lib.nixosSystem {
-          specialArgs = {inherit system;};
-          modules = [
-            ./hosts/thinkpad/configuration.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                users.leana = import ./home;
-                extraSpecialArgs = {inherit unstable;};
-              };
-            }
-          ];
-        };
+      thinkpad = withSystem "aarch64-linux" "nixie";
     };
   };
 }
