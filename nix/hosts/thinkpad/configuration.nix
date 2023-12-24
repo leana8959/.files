d374f2925a9b1ab1bfad34a864f733881bd35ad4
Léana 江 <leana.jiang@icloud.com>
2023-12-24 14:47:52 +0100

    ref: nixos config (removed home.nix)

--- nix/hosts/thinkpad/configuration.nix
+++ nix/hosts/thinkpad/configuration.nix
@@ -1,9 +1,34 @@
 {
-  config,
-  pkgs,
+  nixpkgs,
+  nixunstable,
+  system,
+  lib,
   ...
-}: {
-  # TODO: maybe break these into files
+}: let
+  pkgs = import nixpkgs {
+    inherit system;
+    overlays = [
+      (final: prev: {
+        cmus = prev.cmus.overrideAttrs (old: {
+          patches =
+            (old.patches or [])
+            ++ [
+              (prev.fetchpatch {
+                url = "https://github.com/cmus/cmus/commit/4123b54bad3d8874205aad7f1885191c8e93343c.patch";
+                hash = "sha256-YKqroibgMZFxWQnbmLIHSHR5sMJduyEv6swnKZQ33Fg=";
+              })
+            ];
+        });
+      })
+    ];
+
+    config.allowUnfreePredicate = pkg:
+      builtins.elem (lib.getName pkg) [
+        "discord"
+      ];
+  };
+  unstable = import nixunstable {inherit system;};
+in {
   imports = [
     ./hardware-configuration.nix
   ];
@@ -48,11 +73,82 @@
   };
   console.keyMap = "dvorak";
 
+  # TODO: break this into modules
   users.users.leana = {
     isNormalUser = true;
     description = "leana";
     extraGroups = ["networkmanager" "wheel"];
-    packages = [];
+    packages = with pkgs; [
+      # text/editors
+      helix
+      gnused
+      neovim
+      ripgrep
+      vim
+      tmux
+
+      # nix
+      nil
+      alejandra
+
+      # shell
+      fish
+      # (python39.withPackages (ps: with ps; [beautifulsoup4 requests]))
+      stow
+
+      fd
+      fzf
+      htop
+      starship
+      tree
+      vivid
+      rsync
+
+      # fancy utilities
+      figlet
+      macchina
+      ncdu
+      tldr
+
+      # git related
+      bat
+      delta
+      gnupg
+
+      (nerdfonts.override {
+        fonts = ["CascadiaCode" "JetBrainsMono" "Meslo"];
+      })
+
+      asciinema
+      cmus
+      cmusfm
+      hyperfine
+      tea
+      yt-dlp
+      watchexec
+
+      # jdk17
+      # rustup
+      # nodejs_20
+
+      unstable.typst
+
+      # # NOTE: doesn't work
+      # valgrind
+
+      # gdb
+
+      # Window Manager related
+      dmenu
+      xmobar
+      scrot
+      xscreensaver
+      trayer
+      xclip
+
+      # GUI apps
+      # discord
+    ];
   };
 
   #########################
@@ -68,6 +164,10 @@
   programs = {
     gnome-terminal.enable = true;
     firefox.enable = true;
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
   };
 
   ##########
