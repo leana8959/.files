05369389e8cc9c08c8ae1527892b9009a417b214
Léana 江 <leana.jiang@icloud.com>
2023-12-27 23:25:21 +0100

    add(nix): more cjk fonts

--- nix/home/gui.nix
+++ nix/home/gui.nix
@@ -7,6 +7,12 @@
     # fonts
     (nerdfonts.override {fonts = ["CascadiaCode" "JetBrainsMono" "Meslo"];})
     lmodern
+    noto-fonts
+    noto-fonts-lgc-plus
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    noto-fonts-color-emoji
+    noto-fonts-emoji-blob-bin
 
     # Window Manager related
     dmenu
