a812689c9e5bb6aa61da47867120be5145783dba
Léana 江 <leana.jiang@icloud.com>
2023-12-28 18:51:38 +0100

    nix: removed gnome-terminal's custom palette

--- nix/home/gui.nix
+++ nix/home/gui.nix
@@ -54,28 +54,12 @@
   programs.gnome-terminal = {
     enable = true;
     themeVariant = "light";
-    # showMenubar = false;
-    showMenubar = true;
+    showMenubar = false;
     profile."060efe23-3ab4-4c71-8a38-dd13f89b400d" = {
       default = true;
       visibleName = "Atom OneLight";
       audibleBell = false;
       font = "CaskaydiaCove Nerd Font Mono 15";
-      colors = {
-        # NOTE: there's no "black on white to be chosen"
-        foregroundColor = "#000000";
-        backgroundColor = "#FFFFFF";
-        palette = [
-          "#000000" "#000000"
-          "#E75644" "#E75545"
-          "#4DA24B" "#4DA24B"
-          "#DBC28F" "#DBC28F"
-          "#3E74F5" "#3E74F5"
-          "#A925A5" "#B128A6"
-          "#4DA24B" "#4DA24B"
-          "#C7C7C7" "#FFFFFF"
-          ];
-      };
     };
   };
 
