aebc775efa40f586fcbc25a3ffd2630b2724118c
Léana 江 <leana.jiang@icloud.com>
2023-12-28 14:08:04 +0100

    add(nixos): LSPs (mason won't work)

--- nix/home/dev.nix
+++ nix/home/dev.nix
@@ -1,12 +1,24 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    # editors and utils
+    # Editors and utils
     tmux
     neovim
     vim
     hyperfine
     watchexec
     tea
+
+    # Generic LSPs
+    vscode-langservers-extracted # HTML/CSS/JSON/ESLint
+    nodePackages.bash-language-server # Bash
+    marksman # Markdown
+    nodePackages.pyright # Python
+    taplo # TOML
+    texlab # LaTeX
+    typescript # TypeScript
+    nodePackages.vim-language-server # Vim Script
+    typst-lsp # Typst
+    lua-language-server # Lua
 
     # git
     git
@@ -17,8 +29,21 @@
 
     nil
     alejandra
+    nixfmt
 
-    (python39.withPackages (ps: with ps; [beautifulsoup4 requests]))
+    (python39.withPackages (ps:
+      with ps; [
+        beautifulsoup4
+        requests
+
+        python-lsp-server
+        rope
+        pyflakes
+        mccabe
+        pycodestyle
+        pydocstyle
+        autopep8
+      ]))
     jdk17
     rustup
     nodejs_20
