c65a3ff3f12edccea0bf0f7fd60b5a069281732e
Léana 江 <leana.jiang@icloud.com>
2023-12-28 18:18:25 +0100

    add(gnome-terminal): settings

--- nix/home/gui.nix
+++ nix/home/gui.nix
@@ -1,4 +1,13 @@
 { pkgs, wired, ... }: {
+
+  home.pointerCursor = {
+    x11.enable = true;
+    gtk.enable = true;
+    name = "Adwaita";
+    package = pkgs.gnome.adwaita-icon-theme;
+    size = 32;
+  };
+
   home.packages = with pkgs; [
     # fonts
     (nerdfonts.override { fonts = [ "CascadiaCode" "JetBrainsMono" "Meslo" ]; })
@@ -16,7 +25,6 @@
     dmenu
     xmobar
     scrot
-    xscreensaver
     xclip
     wired.wired
 
@@ -29,17 +37,46 @@
     # productivity
     bitwarden
     sioyek
-
-    # util
-    # TODO: use the programs option to change profiles
-    gnome.gnome-terminal
+    thunderbird
   ];
 
-  home.pointerCursor = {
-    x11.enable = true;
-    gtk.enable = true;
-    name = "Adwaita";
-    package = pkgs.gnome.adwaita-icon-theme;
-    size = 32;
+  services.xscreensaver = {
+    enable = true;
+    settings = {
+      fade = false;
+      unfade = false;
+      timeout = "0:10:00";
+      lock = true;
+      mode = "unknownpleasures";
+    };
   };
+
+  programs.gnome-terminal = {
+    enable = true;
+    themeVariant = "light";
+    # showMenubar = false;
+    showMenubar = true;
+    profile."060efe23-3ab4-4c71-8a38-dd13f89b400d" = {
+      default = true;
+      visibleName = "Atom OneLight";
+      audibleBell = false;
+      font = "CaskaydiaCove Nerd Font Mono 15";
+      colors = {
+        # NOTE: there's no "black on white to be chosen"
+        foregroundColor = "#000000";
+        backgroundColor = "#FFFFFF";
+        palette = [
+          "#000000" "#000000"
+          "#E75644" "#E75545"
+          "#4DA24B" "#4DA24B"
+          "#DBC28F" "#DBC28F"
+          "#3E74F5" "#3E74F5"
+          "#A925A5" "#B128A6"
+          "#4DA24B" "#4DA24B"
+          "#C7C7C7" "#FFFFFF"
+          ];
+      };
+    };
+  };
+
 }
