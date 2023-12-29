e78011b0dae501248f0d033a02cb981a3bf6612f
Léana 江 <leana.jiang@icloud.com>
2023-12-29 10:44:51 +0100

    add(nix): helvum

---
+++ nix/hosts/thinkpad/audio.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }: {
+  sound = {
+    enable = true;
+    mediaKeys.enable = true;
+  };
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [ helvum ];
+}
