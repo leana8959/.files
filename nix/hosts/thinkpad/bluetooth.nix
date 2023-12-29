27dfed3809b7d0d4c5535efa4d33afcaaea02668
Léana 江 <leana.jiang@icloud.com>
2023-12-29 18:16:30 +0100

    nixos: removed bluetooth A2DP mode for the moment

--- nix/hosts/thinkpad/bluetooth.nix
+++ nix/hosts/thinkpad/bluetooth.nix
@@ -1,14 +1,15 @@
 {...}: {
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
-  environment.etc = {
-    "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-      bluez_monitor.properties = {
-        ["bluez5.enable-sbc-xq"] = true,
-        ["bluez5.enable-msbc"] = true,
-        ["bluez5.enable-hw-volume"] = true,
-        ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-      }
-    '';
-  };
+  # FIXME: this breaks the handsfree microphone mode for XM4
+  # environment.etc = {
+  #   "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+  #     bluez_monitor.properties = {
+  #       ["bluez5.enable-sbc-xq"] = true,
+  #       ["bluez5.enable-msbc"] = true,
+  #       ["bluez5.enable-hw-volume"] = true,
+  #       ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+  #     }
+  #   '';
+  # };
 }
