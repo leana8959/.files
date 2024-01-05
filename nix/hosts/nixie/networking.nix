887a9ab8c3eca6770cb7f18924b7afff3d817bc1
Léana 江 <leana.jiang@icloud.com>
2024-01-05 16:24:07 +0100

    nix: use nfs instead

--- nix/hosts/nixie/networking.nix
+++ nix/hosts/nixie/networking.nix
@@ -1,7 +1,6 @@
 {
   config,
   hostname,
-  pkgs,
   lib,
   ...
 }: {
@@ -11,15 +10,16 @@
 
   services.openssh.enable = true;
 
-  environment.systemPackages = [pkgs.cifs-utils];
   fileSystems."/mnt/data" = {
-    device = "//10.0.0.20/data";
-    fsType = "cifs";
-    options = let
-      prevent_hanging = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-      auth = "credentials=${config.age.secrets.truenas_smb.path}";
-      uid = "uid=${toString config.users.users.leana.uid}";
-    in ["${prevent_hanging},${auth},${uid}"];
+    device = "10.0.0.20:/mnt/mainPool/data";
+    fsType = "nfs";
+    options = [
+      "noauto"
+      "x-systemd.automount"
+      "x-systemd.idle-timeout=60"
+      "x-systemd.device-timeout=5s"
+      "x-systemd.mount-timeout=5s"
+    ];
   };
 
   networking.wireless = {
