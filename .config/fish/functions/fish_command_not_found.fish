441c715c45cd1a39c1f77c1a37392f82c8841ee7
Léana 江 <leana.jiang@icloud.com>
2024-01-06 18:37:07 +0100

    me no french

--- .config/fish/functions/fish_command_not_found.fish
+++ .config/fish/functions/fish_command_not_found.fish
@@ -3,11 +3,11 @@ function fish_command_not_found
     set hour (date +"%H")
     set user (whoami)
 
-    set words                              \
-            "Are you drunk ??"             \
-            "Il est seulement $hour heure" \
-            "Reveille-toi, $user"          \
-            "Magique de vim !"             \
+    set words                               \
+            "Are you drunk ??"              \
+            "Il est seulement $hour heures" \
+            "Reveille-toi, $user"           \
+            "Magique de vim !"              \
             "As-tu ecris ton journal aujourd'hui ?"
 
     set choice $words[(random 1 (count $words))]
