d64b70137f6128929f1d3f1d786cee30d1425b15
Léana 江 <leana.jiang@icloud.com>
2024-01-12 11:37:21 +0100

    nix: stop using gdm

--- nix/hosts/nixie/gui.nix
+++ nix/hosts/nixie/gui.nix
@@ -1,26 +1,30 @@
 {...}: {
-  services.xserver.enable = true;
+  services = {
+    xserver.enable = true;
 
-  services.xserver = {
-    autoRepeatDelay = 300;
-    autoRepeatInterval = 40;
-  };
+    xserver = {
+      autoRepeatDelay = 300;
+      autoRepeatInterval = 40;
+    };
 
-  services.xserver = {
-    displayManager.gdm.enable = true;
-    windowManager.xmonad = {
+    xserver.desktopManager.wallpaper.mode = "fill";
+
+    xserver.windowManager.xmonad = {
       enable = true;
       enableContribAndExtras = true;
-      extraPackages = hp: with hp; [neat-interpolation]; # Q: needed ?
+      extraPackages = hp: with hp; [neat-interpolation];
     };
-  };
 
-  services.xserver.libinput = {
-    mouse = {
-      naturalScrolling = true;
-      accelSpeed = "-0.5";
+    xserver.libinput = {
+      mouse = {
+        naturalScrolling = true;
+        accelSpeed = "-0.5";
+      };
+      touchpad = {
+        naturalScrolling = true;
+        tapping = false;
+      };
     };
-    touchpad = {naturalScrolling = true;};
   };
 
   programs.light.enable = true;
