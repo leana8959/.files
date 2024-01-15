83eaf476068380be6bbe24c5027d13a860dc5839
Léana 江 <leana.jiang@icloud.com>
2024-01-15 21:30:01 +0100

    ref(home-manager): oracle

--- nix/home/leana@oracle/default.nix
+++ nix/home/leana@oracle/default.nix
@@ -1,35 +1,12 @@
-{
-  pkgs,
-  unstable,
-  ...
-}: {
-  programs.home-manager.enable = true;
-
-  imports = [./dev.nix];
-
-  home = {
-    username = "ubuntu";
-    homeDirectory = "/home/ubuntu";
-    stateVersion = "23.11";
-  };
+{pkgs, ...}: {
+  imports = [../common];
 
   home.packages = with pkgs; [
-    # shell and script dependencies
-    fish
-    figlet
-    gnused
-    starship
-    stow
-    ripgrep
-    fd
-    fzf
-    vivid
-    gcc
+    nodePackages.bash-language-server # Bash
+    nodePackages.vim-language-server # Vim Script
+    lua-language-server # Lua
 
-    # utils
-    btop
-    tree
-    rsync
-    tldr
+    nil
+    alejandra
   ];
 }
