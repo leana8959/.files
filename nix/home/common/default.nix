eb7686af7973b64e5dd6640b9e159966c8b01dcc
Léana 江 <leana.jiang@icloud.com>
2024-01-19 19:13:45 +0100

    add(nix): nixify fish config

--- nix/home/common/default.nix
+++ nix/home/common/default.nix
@@ -8,19 +8,93 @@
     homeDirectory = lib.mkDefault "/home/leana";
     stateVersion = "23.11";
   };
-  programs = {
+
+  # TODO: potentially drop legacy support
+  programs = let
+    inherit (builtins) readFile foldl' map listToAttrs concatMap;
+  in {
     home-manager.enable = true;
     direnv = {
       enable = true;
       nix-direnv.enable = true;
     };
+    starship = {
+      enable = true;
+      enableFishIntegration = true;
+      settings = fromTOML (readFile ../../../.config/starship.toml);
+    };
+    fish = {
+      enable = true;
+      shellInit = readFile ./fish/shellInit.fish;
+      interactiveShellInit = let
+        readGlobalFishConfigs = ns:
+          foldl' (l: r: l + "\n" + r) ""
+          (
+            map
+            (n: readFile ../../../.config/fish/conf.d/${n}.fish)
+            ns
+          );
+      in
+        readFile ./fish/interactiveShellInit.fish
+        + readGlobalFishConfigs [
+          "alias"
+          "bind"
+          "colorscheme"
+          "locale"
+        ];
+      functions = let
+        makeFishFunctions = ns:
+          listToAttrs
+          (
+            concatMap
+            (n: [
+              {
+                name = n;
+                value = {body = readFile ../../../.config/fish/functions/${n}.fish;};
+              }
+            ])
+            ns
+          );
+      in
+        makeFishFunctions [
+          "clone_to_repos"
+          "file_extension"
+          "file_mantissa"
+          "fish_command_not_found"
+          "fish_greeting"
+          "fish_remove_path"
+          "install_fisher"
+          "largest-objects-in-repo"
+          "snakecase"
+          "timestamp"
+          "tmux_attach"
+          "tmux_home"
+          "tmux_last"
+          "tmux_sessionizer"
+          "update_dotfiles"
+        ];
+      plugins = let
+        makeFishPlugins = ns: (concatMap
+          (n: [
+            {
+              name = n;
+              src = pkgs.fishPlugins.${n};
+            }
+          ])
+          ns);
+      in
+        makeFishPlugins [
+          "fzf-fish"
+          "colored-man-pages"
+          "sponge"
+        ];
+    };
   };
+
   home.packages = with pkgs; [
     # shell and script dependencies
-    fish
     figlet
     gnused
-    starship
     stow
     ripgrep
     fd
---
+++ nix/home/common/fish/interactiveShellInit.fish
@@ -0,0 +1,6 @@
+# vi cursor style
+fish_vi_key_bindings
+set fish_cursor_default block
+set fish_cursor_insert line
+set fish_cursor_replace_one underscore
+set fish_cursor_visual block
