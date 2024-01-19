84283c9e00b167e97f73ef7f3458acac8c489cf6
Léana 江 <leana.jiang@icloud.com>
2024-01-19 23:43:40 +0100

    fix(nix): tmux calls correct fish, reload hack

--- nix/home/common/tmux/default.nix
+++ nix/home/common/tmux/default.nix
@@ -1,8 +1,14 @@
 {pkgs, ...}: {
   home.file = {
     tmux = {
-      source = ./tmux.conf;
       target = ".tmux.conf";
+      text =
+        builtins.readFile ./tmux.conf
+        + ''
+          set -g default-command "${pkgs.fish}/bin/fish" # Use fish
+          set -g default-shell "${pkgs.fish}/bin/fish"
+          bind r source-file "~/.dotfiles/nix/home/common/tmux/default.nix" \; display-message "tmux.conf reloaded."
+        '';
     };
   };
   home.packages = [pkgs.tmux];
