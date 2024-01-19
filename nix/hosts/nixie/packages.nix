8ab950be7564c85ca04ab36f144300c38d7391fc
Léana 江 <leana.jiang@icloud.com>
2024-01-19 21:28:11 +0100

    ref(nix): gnupg options

--- nix/hosts/nixie/packages.nix
+++ nix/hosts/nixie/packages.nix
@@ -16,6 +16,10 @@
   programs.gnupg.agent = {
     enable = true;
     pinentryFlavor = "curses";
+    settings = {
+      default-cache-ttl = 1209600;
+      max-cache-ttl = 1209600;
+    };
   };
   programs.dconf.enable = true;
   services.gnome.gnome-keyring.enable = true;
