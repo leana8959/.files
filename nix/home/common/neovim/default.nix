035f7c850e5d6f5fbe8f73d401d21ae7b26f4501
Léana 江 <leana.jiang@icloud.com>
2024-01-22 19:40:30 +0100

    nvim: stop using nix for nvim
    I just wanna work, I don't wanna nix

--- nix/home/common/neovim/default.nix
+++ nix/home/common/neovim/default.nix
@@ -1,12 +1,3 @@
 {pkgs, ...}: {
-  # TODO: actually use nix ?
-  home.file = {
-    neovim = {
-      recursive = true;
-      source = ./nvim;
-      target = ".config/nvim";
-    };
-  };
-
   home.packages = with pkgs; [neovim];
 }
