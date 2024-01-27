a5b32771f80dc795564ed49c7ef73837a5af6328
Léana 江 <leana.jiang@icloud.com>
2024-01-27 12:03:54 +0100

    ref(nix): cleaned up fish nix config

--- nix/home/common/fish/functions/commit_codewars.fish
+++
@@ -1,4 +0,0 @@
-set name (basename (pwd))
-set scope (basename (dirname (pwd)))
-
-git commit -m "add($scope): $name"
--- nix/home/common/fish/interactiveShellInit.fish
+++
@@ -1,6 +0,0 @@
-# vi cursor style
-fish_vi_key_bindings
-set fish_cursor_default block
-set fish_cursor_insert line
-set fish_cursor_replace_one underscore
-set fish_cursor_visual block
