86792bfd3f49fadc7959b649170b78fdc258ad69
Léana 江 <leana.jiang@icloud.com>
2024-01-28 14:53:59 +0100

    fish: cleaned up sessionizer

--- nix/home/common/fish/functions/tmux_sessionizer.fish
+++ nix/home/common/fish/functions/tmux_sessionizer.fish
@@ -1,25 +1,10 @@
 # Inspired by the one and only primeagen
 set selected \
-    # {{{
     (begin
-        # use fd even thought it's an extra dependency because find is too slow
+        # use fd even though it's an extra dependency because find is too slow
         fd . $REPOS_PATH $UNIV_REPOS_PATH --exact-depth 2 --type d;
         fd . $PLAYGROUND_PATH --exact-depth 1 --type d;
-        fd . $CODEWARS_PATH/Haskell --exact-depth 1 --type d;
-        fd . $CODEWARS_PATH/Rust --exact-depth 1 --type d;
-        fd . $CODEWARS_PATH/Python --exact-depth 1 --type d;
-        fd . $CODEWARS_PATH/C --exact-depth 1 --type d;
-        fd . $CODEWARS_PATH/Shell --exact-depth 1 --type d;
-        fd . $ZEROJUDGE_PATH --exact-depth 1 --type d;
-        echo "play";
-        echo "codewars haskell";
-        echo "codewars rust";
-        echo "codewars python";
-        echo "codewars c";
-        echo "codewars shell";
-        echo "zerojudge c";
     end 2> /dev/null | sed -e "s|^$HOME|~|" | fzf)
-    # }}}
 
 set selected (echo $selected | sed -e "s|^~|$HOME|")
 
@@ -30,7 +15,6 @@ end
 
 switch $selected
 case "play"
-    # {{{
     read -P "Give it a name: " name
     if test -z $name
         return 0
@@ -39,88 +23,6 @@ case "play"
         set selected ~/playground/$name/
         mkdir -p $selected
     end
-    # }}}
-
-case "codewars rust"
-    # {{{
-    read -P "Give it a name: " name
-    if test -z $name
-        return 0
-    else
-        set kata_name (snakecase $name)
-        set selected $CODEWARS_PATH/Rust/$kata_name/
-        cargo new $selected --name codewars_$kata_name --vcs none
-    end
-    # }}}
-
-case "codewars python"
-    # {{{
-    read -P "Give it a name: " name
-    if test -z $name
-        return 0
-    else
-        set name (snakecase $name)
-        set selected $CODEWARS_PATH/Python/$name/
-        mkdir -p $selected
-        touch $selected/main.py
-    end
-    # }}}
-
-case "codewars haskell"
-    # {{{
-    read -P "Give it a name: " name
-    if test -z $name
-        return 0
-    else
-        set name (snakecase $name | tr '_' '-')
-        set selected $CODEWARS_PATH/Haskell/$name
-
-        cd $CODEWARS_PATH/Haskell ; \
-            stack new $name "https://git.earth2077.fr/leana/stack-templates/raw/branch/mistress/codewars.hsfiles" --resolver lts-21.13 ; \
-            prevd
-    end
-    # }}}
-
-case "codewars c"
-    # {{{
-    read -P "Give it a name: " name
-    if test -z $name
-        return 0
-    else
-        set name (snakecase $name)
-        set selected $CODEWARS_PATH/C/$name/
-        mkdir -p $selected
-        touch $selected/main.c
-        echo "run:
-    gcc -I/opt/homebrew/include -L/opt/homebrew/lib -lcriterion main.c
-    ./a.out" > $selected/makefile
-    end
-    # }}}
-
-case "codewars shell"
-    # {{{
-    read -P "Give it a name: " name
-    if test -z $name
-        return 0
-    else
-        set name (snakecase $name)
-        set selected $CODEWARS_PATH/Shell/$name/
-        mkdir -p $selected
-        touch $selected/main.sh
-    end
-    # }}}
-
-case "zerojudge c"
-    # {{{
-    read -P "Give it a name: " name
-    if test -z $name
-        return 0
-    else
-        set selected $ZEROJUDGE_PATH/$name/
-        mkdir -p $selected
-        touch $selected/main.c
-    end
-    # }}}
 end
 
 set session_name (echo $selected | tr . _)
@@ -144,5 +46,3 @@ if [ -z $TMUX ]
 else
     tmux switch-client -t $session_name
 end
-
-# vim:foldmethod=marker:foldlevel=0
