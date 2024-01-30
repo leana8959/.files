6ecbb0bae2a1bbc2da88f42add3687994fb988eb
Léana 江 <leana.jiang@icloud.com>
2024-01-30 22:57:00 +0100

    ref(nix): use home-manager to manage tmux

--- nix/home/common/tmux/default.nix
+++ nix/home/common/tmux/default.nix
@@ -1,15 +1,11 @@
 {pkgs, ...}: {
-  home.file = {
-    tmux = {
-      target = ".tmux.conf";
-      text =
-        builtins.readFile ./tmux.conf
-        + ''
-          set -g default-command "${pkgs.fish}/bin/fish" # Use fish
-          set -g default-shell "${pkgs.fish}/bin/fish"
-          bind r source-file "~/.dotfiles/nix/home/common/tmux/default.nix" \; display-message "tmux.conf reloaded."
-        '';
-    };
+  programs.tmux = {
+    enable = true;
+    extraConfig =
+      ''
+        set -g default-command "${pkgs.fish}/bin/fish" # Use fish
+        set -g default-shell "${pkgs.fish}/bin/fish"
+      ''
+      + builtins.readFile ./tmux.conf;
   };
-  home.packages = [pkgs.tmux];
 }
