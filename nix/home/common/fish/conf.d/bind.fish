9a260600eb8d042567e9993bc5d0ff634b1f3e14
Léana 江 <leana.jiang@icloud.com>
2024-01-30 21:59:10 +0100

    fish: simplified shell init script

--- nix/home/common/fish/conf.d/bind.fish
+++ nix/home/common/fish/conf.d/bind.fish
@@ -2,3 +2,10 @@ for mode in default insert
     bind --mode $mode \cg tmux_home
     bind --mode $mode \cf tmux_sessionizer
 end
+
+# vi cursor style
+fish_vi_key_bindings
+set fish_cursor_default block
+set fish_cursor_insert line
+set fish_cursor_replace_one underscore
+set fish_cursor_visual block
--- nix/home/common/fish/conf.d/interactiveShellInit.fish
+++ nix/home/common/fish/conf.d/interactiveShellInit.fish
@@ -1,15 +1,6 @@
-# vi cursor style
-fish_vi_key_bindings
-set fish_cursor_default block
-set fish_cursor_insert line
-set fish_cursor_replace_one underscore
-set fish_cursor_visual block
-
-set -x EDITOR nvim                                    # Default editor
-set -x GPG_TTY (tty)                                  # Set TTY for GPG
-set -x fzf_preview_file_cmd 'delta'                   # fzf preview theme (bat)
-set -x LS_COLORS (vivid -m 24-bit generate one-light) # fd theme
-set fzf_fd_opts --hidden --exclude=.git               # fzf-fish search hidden files
-set -x DELTA_FEATURES +side-by-side                   # delta
-set -x GOPATH ~/.go                                   # gopath
-set -x ANSIBLE_CONFIG ~/.ansible.cfg                  # Ansible path
+# fzf preview theme (use delta instead of bat)
+set fzf_preview_file_cmd 'delta'
+# fd uses LS_COLORS
+set LS_COLORS (vivid -m 24-bit generate one-light)
+# fzf-fish search hidden files
+set fzf_fd_opts --hidden --exclude=.git
