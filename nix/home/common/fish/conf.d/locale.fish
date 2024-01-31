cb9740c904d0fe7df348b3566bf8bb018e592dbc
Léana 江 <leana.jiang@icloud.com>
2024-01-31 12:38:12 +0100

    ref(fish): no need to use global variables

--- nix/home/common/fish/conf.d/locale.fish
+++ nix/home/common/fish/conf.d/locale.fish
@@ -1,14 +1,14 @@
 # zh_TW
-# set -gx LANG zh_TW.UTF-8
-# set -gx LANGUAGE zh_TW
-# set -gx LC_ALL zh_TW.UTF-8
+# set -x LANG zh_TW.UTF-8
+# set -x LANGUAGE zh_TW
+# set -x LC_ALL zh_TW.UTF-8
 
 # fr_FR
-# set -gx LANG fr_FR.UTF-8
-# set -gx LANGUAGE fr_FR
-# set -gx LC_ALL fr_FR.UTF-8
+# set -x LANG fr_FR.UTF-8
+# set -x LANGUAGE fr_FR
+# set -x LC_ALL fr_FR.UTF-8
 
 # en_US
-set -gx LANG en_US.UTF-8
-set -gx LANGUAGE en_US
-set -gx LC_ALL en_US.UTF-8
+set -x LANG en_US.UTF-8
+set -x LANGUAGE en_US
+set -x LC_ALL en_US.UTF-8
