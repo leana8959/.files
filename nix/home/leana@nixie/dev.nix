67b6e5d462c7aad9837abf9e935d80d046dee684
Léana 江 <leana.jiang@icloud.com>
2024-02-03 17:31:49 +0100

    ref(nix): update dev toolchains

--- nix/home/leana@nixie/dev.nix
+++ nix/home/leana@nixie/dev.nix
@@ -1,57 +1,37 @@
 {
   pkgs,
   mypkgs,
+  unstable,
   ...
 }: {
-  home.packages = let
-    utils = with pkgs; [
-      hyperfine
-      watchexec
-      tea
-      tokei
-      gnumake
-    ];
+  home.packages = with pkgs; [
+    # Tools
+    hyperfine
+    watchexec
+    tea
+    tokei
+    gnumake
 
-    lsps = with pkgs; [
-      vscode-langservers-extracted # HTML/CSS/JSON/ESLint
-      shellcheck
-      nodePackages.bash-language-server
-      marksman
-      nodePackages.pyright
-      taplo
-      nodePackages.vim-language-server
-      lua-language-server
-    ];
+    # LSPs
+    vscode-langservers-extracted # HTML/CSS/JSON/ESLint
+    shellcheck
+    nodePackages.bash-language-server
+    marksman
+    nodePackages.pyright
+    taplo
+    nodePackages.vim-language-server
+    lua-language-server
+    fnlfmt
 
-    nix = with pkgs; [
-      nil
-      alejandra
-    ];
+    # nix
+    nil
+    alejandra
 
-    pythonToolchain = [
-      (pkgs.python39.withPackages (ps:
-        with ps; [
-          beautifulsoup4
-          requests
-
-          python-lsp-server
-          rope
-          pyflakes
-          mccabe
-          pycodestyle
-          pydocstyle
-          autopep8
-        ]))
-    ];
-  in
-    [
-      # University stuff
-      # unstable.opam # maybe I'll need this
-      mypkgs.logisim-evolution
-      pkgs.rars
-    ]
-    ++ utils
-    ++ lsps
-    ++ nix
-    ++ pythonToolchain;
+    # University stuff
+    unstable.opam
+    unstable.cargo
+    mypkgs.logisim-evolution
+    mypkgs.necrolib
+    pkgs.rars
+  ];
 }
