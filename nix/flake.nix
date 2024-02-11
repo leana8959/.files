a3e2c0397f34c2d5c07bda926b08a83c31f4575c
Léana 江 <leana.jiang@icloud.com>
2024-02-11 13:17:33 +0100

    ref(nix): lib.nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -40,20 +40,14 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixunstable.url = "github:nixos/nixpkgs/nixos-unstable";
-
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     wired.url = "github:Toqozz/wired-notify";
-
     agenix.url = "github:ryantm/agenix/0.15.0";
-
     nixnur.url = "github:nix-community/NUR";
-
     opam-nix.url = "github:tweag/opam-nix";
-
     audio-lint.url = "git+https://git.earth2077.fr/leana/audio-lint";
   };
 }
