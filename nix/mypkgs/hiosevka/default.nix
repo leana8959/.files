e1d580c0a10489831e840876b8e56e46fe692db2
Léana 江 <leana.jiang@icloud.com>
2024-02-20 21:28:58 +0100

    ref(nix): changed iosevka build script: removed --name flag

--- nix/mypkgs/hiosevka/default.nix
+++ nix/mypkgs/hiosevka/default.nix
@@ -7,6 +7,8 @@
       # credits:
       # https://github.com/NixOS/nixpkgs/issues/44329#issuecomment-1231189572
       # https://github.com/NixOS/nixpkgs/issues/44329#issuecomment-1544597422
+      # long font names is not problematic
+      # https://github.com/ryanoasis/nerd-fonts/issues/1018#issuecomment-1953555781
       name = "${font.name}-NerdFont";
       src = font;
       nativeBuildInputs = [unstable.nerd-font-patcher pkgs.parallel];
@@ -14,7 +16,6 @@
         mkdir -p nerd-font
         find \( -name \*.ttf -o -name \*.otf \) | parallel nerd-font-patcher \
             {} \
-            --name {/.}-NF \
             --use-single-width-glyphs \
             --careful \
             --complete \
