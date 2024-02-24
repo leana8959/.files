2f067b8798419734b57b1f0bf1a52ba20c3913e6
Léana 江 <leana.jiang@icloud.com>
2024-02-24 17:44:56 +0100

    ref(nix): refactored out mkNerdFont
    Time to make other customized fonts?

---
+++ nix/mypkgs/mkNerdFont.nix
@@ -0,0 +1,37 @@
+{
+  pkgs,
+  unstable,
+}: {
+  font,
+  extraArgs ? [],
+  useDefaultsArgs ? true,
+}:
+pkgs.stdenv.mkDerivation {
+  /*
+  Credits:
+  https://github.com/NixOS/nixpkgs/issues/44329#issuecomment-1231189572
+  https://github.com/NixOS/nixpkgs/issues/44329#issuecomment-1544597422
+
+  long font names is not problematic:
+  https://github.com/ryanoasis/nerd-fonts/issues/1018#issuecomment-1953555781
+  */
+  name = "${font.name}-NerdFont";
+  src = font;
+  nativeBuildInputs = [unstable.nerd-font-patcher pkgs.parallel];
+  buildPhase = let
+    args = builtins.concatStringsSep " " extraArgs;
+    defArgs =
+      if useDefaultsArgs
+      then builtins.concatStringsSep " " ["--careful" "--complete" "--quiet" "--no-progressbars"]
+      else "";
+  in ''
+    mkdir -p nerd-font
+    find \( -name \*.ttf -o -name \*.otf \) | parallel nerd-font-patcher {} \
+        --outputdir nerd-font ${defArgs} ${args}
+  '';
+  installPhase = ''
+    fontdir="$out"/share/fonts/truetype
+    install -d "$fontdir"
+    install nerd-font/* "$fontdir"
+  '';
+}
