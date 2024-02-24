86b8c63abf18f4f5dd1d45aa7161936226ef45c1
Léana 江 <leana.jiang@icloud.com>
2024-02-24 01:09:11 +0100

    nvim: treesitter ignore markdown

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,7 +1,7 @@
 require("nvim-treesitter.configs").setup {
 
     ensure_installed = {},
-    ignore_install = { "typst" },
+    ignore_install = { "typst", "markdown" },
     sync_install = false,
     auto_install = true,
     highlight = { enable = true },
