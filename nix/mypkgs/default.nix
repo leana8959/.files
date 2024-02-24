080faba2896a50151f32ad7d453cb36182613eca
Léana 江 <leana.jiang@icloud.com>
2024-02-24 17:44:56 +0100

    ref(nix): refactored out mkNerdFont
    Time to make other customized fonts?

--- nix/mypkgs/default.nix
+++ nix/mypkgs/default.nix
@@ -4,7 +4,9 @@
   system,
   opam-nix,
   ...
-}: {
+}: let
+  mkNerdFont = import ./mkNerdFont.nix {inherit pkgs unstable;};
+in {
   logisim-evolution = import ./logisim-evolution.nix {inherit pkgs;};
 
   necrolib = import ./necrolib.nix {
@@ -13,7 +15,7 @@
   };
 
   inherit
-    (import ./hiosevka {inherit pkgs unstable;})
+    (import ./hiosevka {inherit pkgs unstable mkNerdFont;})
     hiosevka
     hiosevka-nerd-font-mono
     hiosevka-nerd-font-propo
