123b5ae7430a716d87409ec4cad94dfee847719a
Léana 江 <leana.jiang@icloud.com>
2024-02-26 17:10:05 +0100

    fish: updated abbrs

--- nix/home/common/fish/aliasesAbbrs.nix
+++ nix/home/common/fish/aliasesAbbrs.nix
@@ -31,9 +31,9 @@
     xp = "cd ~/.dotfiles/.config/xmonad && $EDITOR xmonad.hs && prevd";
 
     # Home-Manager / NixOS
-    ns = "sudo nixos-rebuild switch -L --flake ~/.dotfiles/nix#nixie";
+    ns = "sudo nixos-rebuild switch -L --flake ~/.dotfiles/nix#carbon";
     hp = "cd ~/.dotfiles/.config/home-manager && $EDITOR flake.nix && prevd";
-    hs = "home-manager switch -L --flake ~/.dotfiles/nix#stardust";
+    hs = "home-manager switch -L --flake ~/.dotfiles/nix#bismuth";
     nsh = "nix-shell -p";
   };
   abbrsLinux = {
