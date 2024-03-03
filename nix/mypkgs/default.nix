b58d63a8c0fac746ee779d8239108e4c93c0f502
Léana 江 <leana.jiang@icloud.com>
2024-03-03 14:35:34 +0100

    ref(nix): reduce coupling in hiosevka

--- nix/mypkgs/default.nix
+++ nix/mypkgs/default.nix
@@ -6,7 +6,7 @@
   ...
 }: let
   mkNerdFont = import ./mkNerdFont.nix {inherit pkgs unstable;};
-in {
+in rec {
   logisim-evolution = import ./logisim-evolution.nix {inherit pkgs;};
 
   necrolib = import ./necrolib.nix {
@@ -14,10 +14,13 @@ in {
     inherit opam-nix;
   };
 
-  inherit
-    (import ./hiosevka {inherit pkgs unstable mkNerdFont;})
-    hiosevka
-    hiosevka-nerd-font-mono
-    hiosevka-nerd-font-propo
-    ;
+  hiosevka = import ./hiosevka {inherit pkgs;};
+  hiosevka-nerd-font-mono = mkNerdFont {
+    font = hiosevka;
+    extraArgs = ["--name {/.}-NFM" "--use-single-width-glyphs"];
+  };
+  hiosevka-nerd-font-propo = mkNerdFont {
+    font = hiosevka;
+    extraArgs = ["--name {/.}-NFP" "--variable-width-glyphs"];
+  };
 }
