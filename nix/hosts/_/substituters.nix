95ff7a8768f4606173dbe9d7a8fa755e259a4988
Léana 江 <leana.jiang@icloud.com>
2024-03-09 23:05:25 +0100

    fix(nix): substituters
    lol this is actually how you use substituters

--- nix/hosts/_/substituters.nix
+++ nix/hosts/_/substituters.nix
@@ -1,12 +1,17 @@
 {
   nix.settings = {
+    substituters = [
+      "https://leana8959.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://llama-cpp.cachix.org"
+    ];
     trusted-substituters = [
-      "https://app.cachix.org/cache/leana8959"
+      "https://leana8959.cachix.org"
       "https://nix-community.cachix.org"
       "https://llama-cpp.cachix.org"
     ];
     trusted-public-keys = [
-      "leana8959.cachix.org-1:0kiyNv2RiR04ldLZRRm7EC3+4slPi364bX8JKzwpv6A="
+      "leana8959.cachix.org-1:CxQSAp8lcgMv8Me459of0jdXRW2tcyeYRKTiiUq8z0M="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "llama-cpp.cachix.org-1:H75X+w83wUKTIPSO1KWy9ADUrzThyGs8P5tmAbkWhQc="
     ];
