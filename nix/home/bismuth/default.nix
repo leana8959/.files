99514c7c5dd54362f5b85bc01deaf046fa0e20dd
Léana 江 <leana.jiang@icloud.com>
2024-03-16 19:18:26 +0100

    add(nix): ghc

--- nix/home/bismuth/default.nix
+++ nix/home/bismuth/default.nix
@@ -8,6 +8,9 @@
   home.homeDirectory = lib.mkForce "/Users/leana";
 
   home.packages = [
+    pkgs.ghc
+    pkgs.haskell-language-server
+
     input.audio-lint
     input.hbrainfuck
     input.prop-solveur
