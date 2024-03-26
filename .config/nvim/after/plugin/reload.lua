ffae6c41dcf2763bfaba61e9fdaefdb0a29c044f
Léana 江 <leana.jiang@icloud.com>
2024-03-26 18:11:12 +0100

    add(nvim): helper to reload curry colorscheme

---
+++ .config/nvim/after/plugin/reload.lua
@@ -0,0 +1,17 @@
+-- helper to reload module
+
+local usercmd = vim.api.nvim_create_user_command
+local map = vim.keymap.set
+local remove = require("plenary.reload").reload_module
+
+usercmd("Reload", function(opts)
+    remove(opts.fargs[1])
+    require(opts.fargs[1])
+end, { nargs = 1 })
+
+usercmd("ReloadCurry", function(_)
+    remove("curry")
+    require("curry").setup()
+end, {})
+
+map("n", "<leader>rc", ":ReloadCurry<CR>")
