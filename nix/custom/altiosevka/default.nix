79a5752a09407d2ec8bdfdd572ea6864b45cdf18
Léana 江 <leana.jiang@icloud.com>
2024-04-15 23:02:40 +0200

    nix: altiosevka

---
+++ nix/custom/altiosevka/default.nix
@@ -0,0 +1,13 @@
+{ iosevka }:
+let
+  pname = "altiosevka";
+in
+(iosevka.overrideAttrs (_: { inherit pname; })).override {
+  set = pname;
+  /* Guide: https://github.com/be5invis/Iosevka/blob/main/doc/custom-build.md
+
+     Use `term` spacing to avoid dashed arrow issue
+     https://github.com/ryanoasis/nerd-fonts/issues/1018
+  */
+  privateBuildPlan = builtins.readFile ./buildplan.toml;
+}
