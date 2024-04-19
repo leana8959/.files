e6e1b5f01274f594227720126f1bc62b11be1800
Léana 江 <leana.jiang@icloud.com>
2024-04-19 18:26:06 +0200

    nix: add more builders

--- nix/hosts/bismuth/builders.nix
+++ nix/hosts/bismuth/builders.nix
@@ -1,6 +1,9 @@
 {
   nix.distributedBuilds = true;
 
+  # NOTE:
+  # https://github.com/NixOS/hydra/issues/584#issuecomment-1901289182
+  # use ssh-ng to "fix" not trusted user issue
   nix.buildMachines = [
     {
       hostName = "carbon";
@@ -8,6 +11,7 @@
       protocol = "ssh";
       sshUser = "leana";
       sshKey = "/Users/leana/.ssh/id_ed25519";
+      speedFactor = 2;
       supportedFeatures = [
         "nixos-test"
         "benchmark"
@@ -19,10 +23,40 @@
     {
       hostName = "oracle";
       system = "aarch64-linux";
-      protocol = "ssh";
+      protocol = "ssh-ng";
       sshUser = "ubuntu";
       sshKey = "/Users/leana/.ssh/id_ed25519";
-      speedFactor = 4;
+      speedFactor = 8;
+      supportedFeatures = [
+        "nixos-test"
+        "benchmark"
+        "big-parallel"
+        "kvm"
+      ];
+      mandatoryFeatures = [ ];
+    }
+    {
+      hostName = "paradize";
+      system = "aarch64-linux";
+      protocol = "ssh-ng";
+      sshUser = "leana";
+      sshKey = "/Users/leana/.ssh/id_ed25519";
+      speedFactor = 1;
+      supportedFeatures = [
+        "nixos-test"
+        "benchmark"
+        "big-parallel"
+        "kvm"
+      ];
+      mandatoryFeatures = [ ];
+    }
+    {
+      hostName = "hydrogen";
+      system = "aarch64-linux";
+      protocol = "ssh-ng";
+      sshUser = "leana";
+      sshKey = "/Users/leana/.ssh/id_ed25519";
+      speedFactor = 1;
       supportedFeatures = [
         "nixos-test"
         "benchmark"
