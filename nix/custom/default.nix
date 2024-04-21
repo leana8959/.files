3f7da20765879da45a7323297a183811020c2a0d
Léana 江 <leana.jiang@icloud.com>
2024-04-21 18:51:11 +0200

    nix: use buildDuneProject instead

--- nix/custom/default.nix
+++ nix/custom/default.nix
@@ -9,7 +9,7 @@ let
 
   logisim-evolution = pkgs.callPackage ./logisim-evolution.nix { };
 
-  necrolib = pkgs.callPackage ./necrolib.nix { inherit opam-nix; };
+  necrolib = pkgs.callPackage ./necrolib.nix { };
 
   hiosevka = pkgs.callPackage ./hiosevka { };
   hiosevka-nerd-font-mono = mkNerdFont {
