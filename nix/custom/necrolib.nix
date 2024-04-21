840f5b9895c2f34dbcd22668754f74491a96cd24
Léana 江 <leana.jiang@icloud.com>
2024-04-21 18:28:02 +0200

    nix: upgraded necrolib

--- nix/custom/necrolib.nix
+++ nix/custom/necrolib.nix
@@ -1,8 +1,8 @@
 {
-  pkgs,
+  fetchurl,
   opam-nix,
   system,
-  version ? "v0.14.9",
+  version ? "v0.15.0",
 }:
 let
   pname = "necrolib";
@@ -11,9 +11,10 @@ let
     "v0.14.7.1" = "sha256-rFYzNFsT7LIXzWxOogoJd9vh+ywI2N1GE77tnYO7keg=";
     "v0.14.8" = "sha256-ooc1DfTf4k9vcR2aU6CYzaGCDy4XvX98tvfzTLCljSc=";
     "v0.14.9" = "sha256-wPOa/08AykXAotQLZ/CfxD0kLTnWHZTiTPW950uBukA=";
+    "v0.15.0" = "sha256-NJArIZMcKUTGqTgYntch9pSFcVe15SQTUD2amsCNXGI=";
   };
 
-  src = pkgs.fetchurl {
+  src = fetchurl {
     url = "https://gitlab.inria.fr/skeletons/necro/-/archive/${version}/necro-${version}.tar.gz";
     hash = hashes.${version};
   };
