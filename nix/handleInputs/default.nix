cbcf77b058e7c7a3461bc46fee1b41780febc4e7
Léana 江 <leana.jiang@icloud.com>
2024-05-05 13:58:00 +0200

    nix: refactored overlays

--- nix/handleInputs/default.nix
+++ nix/handleInputs/default.nix
@@ -1,43 +1,47 @@
 { inputs, ... }:
 {
   perSystem =
-    { system, self', ... }:
     {
-      _module.args = rec {
-        pkgs = import inputs.nixpkgs {
-          inherit system;
-          overlays = [
-            # extend pkgs with other inputs
-            (_: prev: prev.lib.mapAttrs (name: input: input.packages.${system}.default) inputs)
+      system,
+      self',
+      pkgs,
+      ...
+    }:
+    {
+      _module.args.pkgs = import inputs.nixpkgs {
+        inherit system;
+        overlays = [
+          # fallback to other inputs
+          (_: prev: prev.lib.mapAttrs (name: input: input.packages.${system}.default) inputs)
 
-            (_: _: {
-              unstable = import inputs.nixunstable { inherit system; };
-              nur = import inputs.nixnur {
-                inherit pkgs;
-                nurpkgs = pkgs;
-              };
-            })
+          (_: _: {
+            unstable = import inputs.nixunstable { inherit system; };
+            nur = import inputs.nixnur {
+              inherit pkgs;
+              nurpkgs = pkgs;
+            };
+          })
 
-            # extend pkgs with my custom set
-            (_: _: { myPkgs = self'.packages; })
+          # extend pkgs with my custom set
+          (_: _: { myPkgs = self'.packages; })
 
-            # resolve pinned pkg sets as attributes
-            (_: _: {
-              neovim-pin = import inputs.neovim-pin { inherit system; };
-              ghc-pin = import inputs.ghc-pin { inherit system; };
-              alt-ergo-pin = import inputs.alt-ergo-pin {
-                inherit system;
-                config.allowUnfree = true;
-              };
-            })
+          # resolve explicitly pinned pkg sets as attributes
+          (_: _: {
+            neovim-pin = import inputs.neovim-pin { inherit system; };
+            ghc-pin = import inputs.ghc-pin { inherit system; };
+            alt-ergo-pin = import inputs.alt-ergo-pin {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          })
+        ];
+
+        config.allowUnfreePredicate =
+          pkg:
+          builtins.elem (inputs.nixpkgs.lib.getName pkg) [
+            "discord"
+            "languagetool"
           ];
-          config.allowUnfreePredicate =
-            pkg:
-            builtins.elem (inputs.nixpkgs.lib.getName pkg) [
-              "discord"
-              "languagetool"
-            ];
-        };
       };
     };
 }
