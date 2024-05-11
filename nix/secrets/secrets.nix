16bfcf145d185ed16026fb3da966853f7f81bfc7
Léana 江 <leana.jiang@icloud.com>
2024-05-11 12:55:41 +0200

    nix: rekeyed secrets with more keys

--- nix/secrets/secrets.nix
+++ nix/secrets/secrets.nix
@@ -1,12 +1,17 @@
 let
-  leana = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFSXmaAIzatHJL3L0GNK2LU8mmf/gPAhQVZBurNgCx72 leana@nixie";
-  users = [ leana ];
+  carbon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFSXmaAIzatHJL3L0GNK2LU8mmf/gPAhQVZBurNgCx72";
+  bismuth = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBt+MINzxBF8uyFIuz/UvMZe9Ml+qxU0hxxi7UAmUzpc";
+
+  all = [
+    bismuth
+    carbon
+  ];
 in
 {
-  "wpa_password.age".publicKeys = users;
+  "wpa_password.age".publicKeys = all;
 
-  "sshconfig.age".publicKeys = users;
+  "sshconfig.age".publicKeys = all;
 
-  "wireguard_priv.age".publicKeys = users;
-  "wireguard_psk.age".publicKeys = users;
+  "wireguard_priv.age".publicKeys = all;
+  "wireguard_psk.age".publicKeys = all;
 }
