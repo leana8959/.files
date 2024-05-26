dd4489a1a302ea2e30f83b12f8eacc4a238f8e2f
Léana 江 <leana.jiang+git@icloud.com>
2024-05-26 12:40:24 +0200

    nix: guard btop config linking behind btop.enable

--- nix/home/_/btop/default.nix
+++ nix/home/_/btop/default.nix
@@ -1,8 +1,9 @@
+{ config, lib, ... }:
 {
   programs.btop.enable = true;
 
   # Link manually to get the theme along with the config
-  home.file.btop = {
+  home.file.btop = lib.mkIf config.programs.btop.enable {
     recursive = true;
     source = ./btop;
     target = ".config/btop";
