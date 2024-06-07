ff00792337358a15641c95d760ea6b33204bae99
Léana 江 <leana.jiang+git@icloud.com>
2024-06-08 00:44:47 +0200

    nix: tokei devShell

--- nix/devShells/default.nix
+++ nix/devShells/default.nix
@@ -2,22 +2,33 @@
 
 {
   perSystem =
-    { system, pkgs, ... }:
+    { pkgs, ... }:
     {
-      devShells.forgejo = pkgs.mkShell {
-        name = "forgejo";
-        packages = [
-          pkgs.sqlite
+      devShells = {
+        forgejo = pkgs.mkShell {
+          name = "forgejo";
+          packages = [
+            pkgs.sqlite
 
-          pkgs.go
-          pkgs.gopls
-          pkgs.golangci-lint
-          pkgs.golangci-lint-langserver
-          pkgs.gofumpt
+            pkgs.go
+            pkgs.gopls
+            pkgs.golangci-lint
+            pkgs.golangci-lint-langserver
+            pkgs.gofumpt
 
-          pkgs.nodejs_20
-          pkgs.gnumake
-        ];
+            pkgs.nodejs_20
+            pkgs.gnumake
+          ];
+        };
+
+        tokei = pkgs.mkShell {
+          name = "tokei";
+          packages = [
+            pkgs.cargo
+            pkgs.rustc
+            pkgs.rust-analyzer
+          ];
+        };
       };
     };
 }
