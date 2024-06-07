b4f090fa15eaceb8c15d32d115517326243b75cd
Léana 江 <leana.jiang+git@icloud.com>
2024-06-07 23:41:10 +0200

    nix: more linting tools

--- nix/pre-commit/default.nix
+++ nix/pre-commit/default.nix
@@ -7,10 +7,20 @@
       pre-commit = {
         check.enable = true;
         settings.hooks = {
+          # nix
           nixfmt.enable = true;
           nixfmt.package = pkgs.nixfmt-rfc-style;
           statix.enable = true;
           deadnix.enable = true;
+
+          # stylua
+          stylua.enable = true;
+
+          # markdown
+          markdownlint.enable = true;
+
+          # toml
+          taplo.enable = true;
         };
       };
 
