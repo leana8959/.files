7b199819c421d63c99f4d74c9e783d0b4e14624e
Léana 江 <leana.jiang+git@icloud.com>
2024-07-07 14:05:43 +0200

    nix: refactored cmus config

--- nix/home/_/cmus/default.nix
+++ nix/home/_/cmus/default.nix
@@ -14,15 +14,17 @@
     ];
     file.cmus = {
       recursive = true;
-      text =
-        builtins.readFile ./rc
-        + lib.strings.optionalString pkgs.stdenv.isLinux ''
+      text = lib.mkMerge [
+        (builtins.readFile ./rc)
+
+        (lib.mkIf pkgs.stdenv.isLinux ''
           set output_plugin=alsa
-        ''
-        + lib.strings.optionalString pkgs.stdenv.isDarwin ''
+        '')
+        (lib.mkIf pkgs.stdenv.isDarwin ''
           # # distortion fix https://github.com/cmus/cmus/issues/1130#issuecomment-1003324193
           # set output_plugin=ao
-        '';
+        '')
+      ];
       target = "${config.xdg.configHome}/cmus/rc";
     };
   };
