79b75d539f8dc30b858b7b7a9ffd071bd66062d1
Léana 江 <leana.jiang+git@icloud.com>
2024-07-13 00:17:41 +0200

    nix: make layouts a nixosModule

--- nix/nixosModules/default.nix
+++ nix/nixosModules/default.nix
@@ -1,6 +1,10 @@
 {
-  flake.nixosModules._.imports = [
-    ./sudo-conf.nix
-    ./system-nixconf.nix
-  ];
+  flake.nixosModules = {
+    _.imports = [
+      ./sudo-conf.nix
+      ./system-nixconf.nix
+    ];
+
+    layouts = ./layouts;
+  };
 }
