a03338093683b299d6b00635e9eb6665da3b4647
Léana 江 <leana.jiang+git@icloud.com>
2024-07-12 23:33:27 +0200

    nix: expose darwinModules

---
+++ nix/darwinModules/default.nix
@@ -0,0 +1,27 @@
+{
+  flake.darwinModules._ =
+    { pkgs, hostname, ... }:
+    {
+      system.stateVersion = 4;
+      services.nix-daemon.enable = true;
+
+      networking.hostName = hostname;
+      environment.shells = [ pkgs.fish ];
+
+      nix.settings.trusted-users = [ "leana" ];
+
+      nix.gc = {
+        automatic = true;
+        interval.Weekday = 0;
+        options = "--delete-older-than 15d";
+      };
+
+      nix.optimise = {
+        automatic = true;
+        interval.Weekday = 0;
+      };
+
+      # https://github.com/nix-community/home-manager/issues/4026
+      users.users."leana".home = "/Users/leana";
+    };
+}
