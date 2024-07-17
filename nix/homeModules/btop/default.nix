1269e3a2678b6e97c8031c364c66e193869620fe
Léana 江 <leana.jiang+git@icloud.com>
2024-07-18 00:51:21 +0200

    nix: btop: use xdg.cofigFile instead

--- nix/homeModules/btop/default.nix
+++ nix/homeModules/btop/default.nix
@@ -3,9 +3,8 @@
   programs.btop.enable = true;
 
   # Link manually to get the theme along with the config
-  home.file.btop = lib.mkIf config.programs.btop.enable {
+  xdg.configFile.btop = lib.mkIf config.programs.btop.enable {
     recursive = true;
     source = ./btop;
-    target = ".config/btop";
   };
 }
