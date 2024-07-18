22b9a5b46f605e67f42120ad747cd0a88a14ad33
Léana 江 <leana.jiang+git@icloud.com>
2024-07-18 23:16:04 +0200

    nix: refactored locale to a module

--- nix/configurations/host/carbon/locale.nix
+++ nix/configurations/host/carbon/locale.nix
@@ -1,6 +1,5 @@
 { pkgs, ... }:
 {
-  time.timeZone = "Europe/Paris";
   i18n = {
     defaultLocale = "en_US.UTF-8";
     extraLocaleSettings = {
@@ -14,11 +13,6 @@
       LC_TELEPHONE = "fr_FR.UTF-8";
       LC_TIME = "fr_FR.UTF-8";
     };
-    supportedLocales = [
-      "en_US.UTF-8/UTF-8"
-      "fr_FR.UTF-8/UTF-8"
-      "zh_TW.UTF-8/UTF-8"
-    ];
   };
 
   i18n.inputMethod.enabled = "fcitx5";
