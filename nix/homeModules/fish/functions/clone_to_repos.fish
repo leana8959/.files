91bb2dab7492ac59f6b77b13f59bdaa1bfe2d56e
Léana 江 <leana.jiang+git@icloud.com>
2024-07-20 10:43:04 +0200

    fish: updated clone_to_repos

--- nix/homeModules/fish/functions/clone_to_repos.fish
+++ nix/homeModules/fish/functions/clone_to_repos.fish
@@ -1,11 +1,13 @@
 function clone_to_repos
-    if count $argv >/dev/null
-        set name_repo (echo $argv | sed -E 's/.*[:\\/]([^\\/]+)\\/([^\\/]+)(:?.git)?$/\1 \2/')
-        set name (echo $name_repo | cut -d ' ' -f1)
-        set repo (echo $name_repo | cut -d ' ' -f2)
-        mkdir -p $REPOS_PATH/$name
-        git clone $argv $REPOS_PATH/$name/$repo
-    else
-        echo "Please provide a git url"
+
+    if not count $argv >/dev/null
+        echo "Git url needed"
     end
+
+    set name_repo (echo $argv | sed -E 's/.*[:\\/]([^\\/]+)\\/([^\\/]+)(:?.git)?$/\1 \2/')
+    set name (echo $name_repo | cut -d ' ' -f1)
+    set repo (echo $name_repo | cut -d ' ' -f2)
+    mkdir -p $REPOS_PATH/$name
+    git clone $argv $REPOS_PATH/$name/$repo
+
 end
