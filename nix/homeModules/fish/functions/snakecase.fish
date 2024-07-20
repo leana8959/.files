503e9df17d53574a33ff26e5b95dee8f84600094
Léana 江 <leana.jiang+git@icloud.com>
2024-07-20 10:44:27 +0200

    fish: cleanup

--- nix/homeModules/fish/functions/snakecase.fish
+++ nix/homeModules/fish/functions/snakecase.fish
@@ -1,3 +1,5 @@
 function snakecase
+
     echo $argv | sed -E 's/[^A-Za-z0-9_\n]+/_/g' | sed -E 's/__+/_/g' | tr '[:upper:]' '[:lower:]'
+
 end
--- nix/homeModules/fish/functions/update_dotfiles.fish
+++ nix/homeModules/fish/functions/update_dotfiles.fish
@@ -1,8 +1,10 @@
 function update_dotfiles
+
     cd ~/.dotfiles/
     git reset --hard
     git pull --set-upstream origin main
     git submodule update --init
     prevd
     restow
+
 end
