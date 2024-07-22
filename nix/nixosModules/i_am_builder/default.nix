c508c1407e872f832b97fb142483062269eed9f7
Léana 江 <leana.jiang+git@icloud.com>
2024-07-22 16:32:41 +0200

    nix: i_am_builder: set my root key
    systemwide -> systemwide remote build requires this

--- nix/nixosModules/i_am_builder/default.nix
+++ nix/nixosModules/i_am_builder/default.nix
@@ -17,6 +17,7 @@
     # credit:
     # https://github.com/nix-community/srvos/blob/main/nixos/roles/nix-remote-builder.nix
     openssh.authorizedKeys.keys = map (key: ''restrict ${key}'') [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEdzs65v65s5sVEv+BClW7qYb0tWuLOZ4e8lIAActeUq root@bismuth"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBt+MINzxBF8uyFIuz/UvMZe9Ml+qxU0hxxi7UAmUzpc leana@bismuth"
     ];
   };
