d4fd7feaab48c655af526d2de36ae6d74d9fe553
Léana 江 <leana.jiang+git@icloud.com>
2024-07-23 21:19:34 +0200

    updated battery threshold setting

--- nix/configurations/host/carbon/battery.nix
+++ nix/configurations/host/carbon/battery.nix
@@ -1,19 +1,13 @@
-{ pkgs, ... }:
 {
-  systemd.services.battery-charge-threshold = {
+  systemd.services."set-battery-thres" = {
     enable = true;
     description = "Set the battery charge threshold";
-    unitConfig = {
-      After = "multi-user.target";
-      StartLimitBurst = 0;
-    };
-    serviceConfig = {
-      User = "root";
-      Group = "root";
-      Type = "oneshot";
-      Restart = "on-failure";
-      ExecStart = "${pkgs.bash}/bin/bash -c 'echo 70 > /sys/class/power_supply/BAT1/charge_control_start_threshold; echo 80 > /sys/class/power_supply/BAT1/charge_control_end_threshold'";
-    };
+    unitConfig.After = "multi-user.target";
+    script = ''
+      echo 70 > /sys/class/power_supply/BAT1/charge_control_start_threshold
+      echo 80 > /sys/class/power_supply/BAT1/charge_control_end_threshold
+    '';
+    serviceConfig.Restart = "on-failure";
     wantedBy = [ "multi-user.target" ];
   };
 }
