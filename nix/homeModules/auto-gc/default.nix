c329e02050ce4f28ef583fa19b071d9207746f08
Léana 江 <leana.jiang+git@icloud.com>
2024-07-23 21:24:20 +0200

    nix: less aggressive gc, unless is builder

--- nix/homeModules/auto-gc/default.nix
+++ nix/homeModules/auto-gc/default.nix
@@ -1,10 +1,7 @@
-{ pkgs, lib, ... }:
 {
   nix.gc = {
     automatic = true;
-    frequency = lib.mkMerge [
-      (lib.mkIf pkgs.stdenv.isDarwin "daily")
-      (lib.mkIf pkgs.stdenv.isLinux "1 day")
-    ];
+    frequency = "weekly";
+    options = "--delete-older-than 15d";
   };
 }
