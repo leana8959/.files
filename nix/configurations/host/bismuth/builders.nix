a898364c9a892b2d9515a334df56cd20b40161dc
Léana 江 <leana.jiang+git@icloud.com>
2024-07-24 21:48:31 +0200

    nix: updated builders

--- nix/configurations/host/bismuth/builders.nix
+++ nix/configurations/host/bismuth/builders.nix
@@ -4,51 +4,57 @@
   # NOTE:
   # https://github.com/NixOS/hydra/issues/584#issuecomment-1901289182
   # use ssh-ng to "fix" not trusted user issue
-  nix.buildMachines = [
-    {
-      hostName = "carbon";
-      system = "x86_64-linux";
-      protocol = "ssh";
-      sshUser = "nix-remote-builder";
-      speedFactor = 4;
-      supportedFeatures = [
-        "nixos-test"
-        "benchmark"
-        "big-parallel"
-        "kvm"
-      ];
-      mandatoryFeatures = [ ];
-    }
-    {
-      hostName = "hydrogen";
-      system = "aarch64-linux";
-      protocol = "ssh";
-      sshUser = "nix-remote-builder";
-      speedFactor = 2;
-      supportedFeatures = [
-        "nixos-test"
-        "benchmark"
-        "big-parallel"
-        "kvm"
-      ];
-      mandatoryFeatures = [ ];
-    }
-    {
-      hostName = "oracle";
-      system = "aarch64-linux";
-      protocol = "ssh-ng";
-      sshUser = "ubuntu";
-      sshKey = "/Users/leana/.ssh/id_ed25519";
-      speedFactor = 8;
-      supportedFeatures = [
-        "nixos-test"
-        "benchmark"
-        "big-parallel"
-        "kvm"
-      ];
-      mandatoryFeatures = [ ];
-    }
-  ];
+  nix.buildMachines =
+    let
+      def = {
+        hostName = throw "hostName is not defined";
+        system = throw "system is not defined";
+        protocol = "ssh";
+        sshUser = "nix-remote-builder";
+        speedFactor = 1;
+        supportedFeatures = [
+          "nixos-test"
+          "benchmark"
+          "big-parallel"
+          "kvm"
+        ];
+        mandatoryFeatures = [ ];
+      };
+
+      carbon = def // {
+        hostName = "carbon";
+        system = "x86_64-linux";
+        speedFactor = 4;
+      };
+
+      forgejo = def // {
+        hostName = "forgejo";
+        system = "x86_64-linux";
+        speedFactor = 1;
+      };
+
+      hydrogen = def // {
+        hostName = "hydrogen";
+        system = "aarch64-linux";
+        speedFactor = 2;
+      };
+
+      oracle = def // {
+        hostName = "oracle";
+        system = "aarch64-linux";
+        protocol = "ssh-ng";
+        sshUser = "ubuntu";
+        speedFactor = 8;
+      };
+    in
+    [
+      # TODO: rebuilding...
+      # carbon
+
+      forgejo
+      hydrogen
+      oracle
+    ];
 
   # optional, useful when the builder has a faster internet connection than yours
   nix.extraOptions = ''
