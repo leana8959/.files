0fd5f6bc6d982ccd7e5771bfef70c3979ec33b92
Léana 江 <leana.jiang+git@icloud.com>
2024-07-28 23:16:46 +0200

    fish: fix clone_to_repos regex

--- nix/homeModules/fish/functions/clone_to_repos.fish
+++ nix/homeModules/fish/functions/clone_to_repos.fish
@@ -4,7 +4,7 @@ function clone_to_repos
         echo "Git url needed"
     end
 
-    set name_repo (echo $argv | sed -E 's/.*[:\\/]([^\\/]+)\\/([^\\/.]+?)(:?.git)?$/\1 \2/')
+    set name_repo (echo $argv | sed -E 's/.*[:\\/]([^\\/]+)\\/([^\\/]+?)(:?.git)?$/\1 \2/')
     set name (echo $name_repo | cut -d ' ' -f1)
     set repo (echo $name_repo | cut -d ' ' -f2)
     mkdir -p $REPOS_PATH/$name
