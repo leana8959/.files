9845a8a1dd78b1074f91d57b3d58d77af9207fd8
Léana 江 <leana.jiang+git@icloud.com>
2024-07-28 11:34:45 +0200

    nix: cleaned up carbon locale

--- nix/configurations/host/carbon/locale.nix
+++ nix/configurations/host/carbon/locale.nix
@@ -1,20 +1,6 @@
 { pkgs, ... }:
-{
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "fr_FR.UTF-8";
-      LC_IDENTIFICATION = "fr_FR.UTF-8";
-      LC_MEASUREMENT = "fr_FR.UTF-8";
-      LC_MONETARY = "fr_FR.UTF-8";
-      LC_NAME = "fr_FR.UTF-8";
-      LC_NUMERIC = "fr_FR.UTF-8";
-      LC_PAPER = "fr_FR.UTF-8";
-      LC_TELEPHONE = "fr_FR.UTF-8";
-      LC_TIME = "fr_FR.UTF-8";
-    };
-  };
 
+{
   i18n.inputMethod.enabled = "fcitx5";
   i18n.inputMethod.fcitx5.addons = [
     pkgs.fcitx5-chinese-addons
