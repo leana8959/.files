4d33a68e54b8136fc2deda432653f4b22bedbd24
Léana 江 <leana.jiang+git@icloud.com>
2024-07-29 13:06:29 +0200

    fish: dotfiles session

--- nix/homeModules/fish/functions/tmux_sessionizer.fish
+++ nix/homeModules/fish/functions/tmux_sessionizer.fish
@@ -7,6 +7,7 @@ function tmux_sessionizer \
             fd . $PLAYGROUND_PATH --exact-depth 1 --type d
             # Special mode to create a playground
             echo "play"
+            echo "dotfiles"
         end 2> /dev/null | sed -e "s|^$HOME|~|" | fzf)
 
     set selected (echo $selected | sed -e "s|^~|$HOME|")
@@ -16,20 +17,30 @@ function tmux_sessionizer \
         return 0
     end
 
+    # derive session name based on selected path
+    # dots are meaningful in tmux, remove them
+    set session_name (echo $selected | tr . _)
+
+    # fixup special cases, override derived session_name if necessary
+    # this should be id for non-special cases
     switch $selected
         case play
             read -P "Give it a name: " name
-            if test -z "$name"
+            if [ -z "$name" ]
                 return 0
-            else
-                set name (snakecase $name)
-                set selected ~/playground/$name/
-                mkdir -p $selected
             end
+            set name (snakecase "$name")
+
+            set selected ~/playground/$name
+            set session_name (echo $selected | tr . _)
+            mkdir -p $selected
+
+        case dotfiles
+            set selected ~/.dotfiles
+            set session_name dotfiles
     end
 
-    set session_name (echo $selected | tr . _)
-
+    # effects
     __tmux_register_session $session_name
     __tmux_maybe_create $session_name $selected
     __tmux_attach_or_switch $session_name
