b92e58e225eda39a6b5a44f410b01f8dc86294d5
Léana 江 <leana.jiang+git@icloud.com>
2024-07-29 21:06:00 +0200

    allow carbon to connect to my user

--- nix/nixosModules/leana/default.nix
+++ nix/nixosModules/leana/default.nix
@@ -11,6 +11,7 @@
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = config.users.users.root.openssh.authorizedKeys.keys ++ [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBt+MINzxBF8uyFIuz/UvMZe9Ml+qxU0hxxi7UAmUzpc leana@bismuth"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBvP72buxKrTAtu9SxSqd0kzzbGxY7fUwgT100Q0S/Yi leana@carbon"
     ];
   };
 
