0d643856147833adf755ff590537b95e9b2deeaa
Léana 江 <leana.jiang+git@icloud.com>
2024-08-01 09:43:21 +0200

    nix: make my user's allowed ssh keys depend on root

--- nix/nixosModules/leana/default.nix
+++ nix/nixosModules/leana/default.nix
@@ -3,16 +3,13 @@
 {
   nix.settings.trusted-users = [ "leana" ];
 
-  users.users.leana = {
+  users.users."leana" = {
     isNormalUser = true;
     home = "/home/leana";
     description = "Leana";
     group = "leana";
     extraGroups = [ "wheel" ];
-    openssh.authorizedKeys.keys = config.users.users.root.openssh.authorizedKeys.keys ++ [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBt+MINzxBF8uyFIuz/UvMZe9Ml+qxU0hxxi7UAmUzpc leana@bismuth"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBvP72buxKrTAtu9SxSqd0kzzbGxY7fUwgT100Q0S/Yi leana@carbon"
-    ];
+    openssh.authorizedKeys.keys = config.users.users.root.openssh.authorizedKeys.keys;
   };
 
   users.groups.leana = { };
