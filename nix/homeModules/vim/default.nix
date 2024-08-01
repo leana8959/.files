0566a60a468e0856af8217963ad7aeb7c3ff311e
Léana 江 <leana.jiang+git@icloud.com>
2024-08-01 09:55:37 +0200

    nix: refactor home-manager vim

--- nix/homeModules/vim/default.nix
+++ nix/homeModules/vim/default.nix
@@ -1,34 +1,33 @@
 { pkgs, ... }:
+
 {
-  programs.vim =
-    let
-      paramount = pkgs.vimUtils.buildVimPlugin {
-        name = "paramount";
-        src = pkgs.fetchFromGitHub {
-          owner = "owickstrom";
-          repo = "vim-colors-paramount";
-          rev = "a5601d36fb6932e8d1a6f8b37b179a99b1456798";
-          hash = "sha256-j9nMjKYK7bqrGHprYp0ddLEWs1CNMudxXD13sOROVmY=";
+  programs.vim = {
+    enable = true;
+    extraConfig = builtins.readFile ./vimrc;
+    plugins =
+      let
+        vpkgs = pkgs.vimPlugins;
+
+        paramount = pkgs.vimUtils.buildVimPlugin {
+          name = "paramount";
+          src = pkgs.fetchFromGitHub {
+            owner = "owickstrom";
+            repo = "vim-colors-paramount";
+            rev = "a5601d36fb6932e8d1a6f8b37b179a99b1456798";
+            hash = "sha256-j9nMjKYK7bqrGHprYp0ddLEWs1CNMudxXD13sOROVmY=";
+          };
         };
-      };
-    in
-    {
-      enable = true;
-      extraConfig = builtins.readFile ./vimrc;
-      plugins =
-        let
-          vpkgs = pkgs.vimPlugins;
-        in
-        [
-          vpkgs.vim-sleuth
-          vpkgs.vim-surround
-          vpkgs.vim-fugitive
-          vpkgs.vim-commentary
-          vpkgs.undotree
-          vpkgs.tabular
-          vpkgs.vim-wakatime
-          vpkgs.vim-caddyfile
-          paramount
-        ];
-    };
+      in
+      [
+        vpkgs.vim-sleuth
+        vpkgs.vim-surround
+        vpkgs.vim-fugitive
+        vpkgs.vim-commentary
+        vpkgs.undotree
+        vpkgs.tabular
+        vpkgs.vim-wakatime
+        vpkgs.vim-caddyfile
+        paramount
+      ];
+  };
 }
