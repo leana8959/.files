c9323a5cae23adc4870267dd98fc54076deace91
Léana 江 <leana.jiang+git@icloud.com>
2024-08-01 09:56:45 +0200

    nix: refactor user nix.conf

--- nix/homeModules/user-nixconf/default.nix
+++ nix/homeModules/user-nixconf/default.nix
@@ -1,18 +1,10 @@
-# generates user's substituter's config
-# should be used with home-manager
 { pkgs, lib, ... }:
+
 {
   nix = {
     package = lib.mkDefault pkgs.nixVersions.nix_2_21;
     settings = {
       allow-import-from-derivation = "true";
-      /*
-        substituters can only be used by users that are trusted by nix -> nix trusts the user to do it right
-        trusted-substituters can be used by any user -> nix trusts everything the substituter provides
-
-        "In addition, each store path should be trusted as described in trusted-public-keys"
-        -> keys for everything
-      */
       extra-substituters = [
         "https://nix-community.cachix.org"
         "https://leana8959.cachix.org"
