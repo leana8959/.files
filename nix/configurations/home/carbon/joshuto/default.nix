fb53c4fec0f0861c1c60aea9b43ac7275f68de67
Léana 江 <leana.jiang+git@icloud.com>
2024-08-08 12:43:49 +0200

    nix: completed joshuto configuration

--- nix/configurations/home/carbon/joshuto/default.nix
+++ nix/configurations/home/carbon/joshuto/default.nix
@@ -4,15 +4,20 @@
 
 {
 
-  home.packages = [ pkgs.joshuto ];
+  home.packages = [
+    pkgs.joshuto
+
+    # preview deps
+    pkgs.file
+  ];
 
   xdg.configFile = {
     # NOTE: NO DEFAULT OR INHERITED VALUES FROM A DEFAULT CONFIG
 
-    # "joshuto/preview_file.sh" = {
-    #   source = ./preview_file.sh;
-    #   executable = true;
-    # };
+    "joshuto/preview_file.sh" = {
+      source = ./preview_file.sh;
+      executable = true;
+    };
 
     "joshuto/joshuto.toml".source = ./joshuto.toml;
     "joshuto/keymap.toml".source = ./keymap.toml;
