f8a1c85c33432700344ad2e0c5c16d01a723af32
Léana 江 <leana.jiang+git@icloud.com>
2024-08-13 15:36:12 +0200

    carbon: scream at me when my battery is low

--- nix/configurations/host/carbon/battery.nix
+++ nix/configurations/host/carbon/battery.nix
@@ -1,3 +1,5 @@
+{ pkgs, lib, ... }:
+
 {
   systemd.services."set-battery-thres" = {
     enable = true;
@@ -14,5 +16,33 @@
   services.logind = {
     lidSwitch = "hibernate";
     lidSwitchExternalPower = "ignore";
+  };
+
+  systemd.services."battery-notify" = {
+    enable = true;
+    description = "Scream when battery is dying";
+    startAt = [ "*:0/5" ];
+    unitConfig.After = "multi-user.target";
+    serviceConfig.ExecStart =
+      let
+        script = pkgs.writeShellApplication {
+          name = "battery-notify";
+          runtimeInputs = [
+            pkgs.bc
+            pkgs.libnotify
+          ];
+          text = ''
+            battery="/sys/class/power_supply/BAT1"
+            thres="0.2"
+            isLow=$(echo "($(cat $battery/energy_now) / $(cat $battery/energy_full)) < $thres" | bc -l)
+
+            if [ "$isLow" -eq 1 ]; then
+              echo "You're battery level is below $thres"
+              notify-send -u critical "Battery Low" "Please charge your battery"
+            fi
+          '';
+        };
+      in
+      lib.getExe script;
   };
 }
