1862e5e7b4a4b3f03b17eeb9dbaed4d6e586365a
Léana 江 <leana.jiang+git@icloud.com>
2024-08-14 06:57:14 +0200

    carbon: wpa_supplicant priority setup

--- nix/configurations/host/carbon/networking.nix
+++ nix/configurations/host/carbon/networking.nix
@@ -7,24 +7,37 @@
     enable = true;
     userControlled.enable = true;
     environmentFile = config.age.secrets.wpa_password.path;
-    # To add networks: https://nixos.wiki/wiki/Wpa_supplicant
-    networks = {
-      "HiddenParadize@Earth2077".psk = "@HOME@";
-      "iPhone de Léana 江".psk = "@PHONE@";
-      eduroam = {
-        authProtocols = [ "WPA-EAP" ];
-        auth = ''
-          pairwise=CCMP
-          group=CCMP TKIP
-          eap=PEAP
-          ca_cert="${./certs/universite_de_rennes.pem}"
-          identity="@EDUROAM_ID@"
-          altsubject_match="DNS:radius.univ-rennes1.fr;DNS:radius1.univ-rennes1.fr;DNS:radius2.univ-rennes1.fr;DNS:vmradius-psf1.univ-rennes1.fr;DNS:vmradius-psf2.univ-rennes1.fr"
-          phase2="auth=MSCHAPV2"
-          password="@EDUROAM_PSK_L@&@EDUROAM_PSK_R@"
-          anonymous_identity="anonymous@univ-rennes.fr"
-        '';
-      };
-    };
+    networks =
+      let
+        ordered =
+          nss:
+          lib.trivial.pipe nss [
+            lib.lists.reverseList
+            (lib.lists.imap0 (i: lib.mapAttrs (_: n: n // { priority = i; })))
+            lib.mergeAttrsList
+          ];
+      in
+      ordered [
+        # first in list is tried first
+        { "HiddenParadize@Earth2077".psk = "@HOME@"; }
+        { "_SNCF_WIFI_INOUI" = { }; }
+        {
+          eduroam = {
+            authProtocols = [ "WPA-EAP" ];
+            auth = ''
+              pairwise=CCMP
+              group=CCMP TKIP
+              eap=PEAP
+              ca_cert="${./certs/universite_de_rennes.pem}"
+              identity="@EDUROAM_ID@"
+              altsubject_match="DNS:radius.univ-rennes1.fr;DNS:radius1.univ-rennes1.fr;DNS:radius2.univ-rennes1.fr;DNS:vmradius-psf1.univ-rennes1.fr;DNS:vmradius-psf2.univ-rennes1.fr"
+              phase2="auth=MSCHAPV2"
+              password="@EDUROAM_PSK_L@&@EDUROAM_PSK_R@"
+              anonymous_identity="anonymous@univ-rennes.fr"
+            '';
+          };
+        }
+        { "iPhone de Léana 江".psk = "@PHONE@"; }
+      ];
   };
 }
