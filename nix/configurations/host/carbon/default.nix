84374186d1b1bd9baae22be58b2a263042fe668d
Léana 江 <leana.jiang+git@icloud.com>
2024-08-16 20:30:38 +0200

    carbon: don't mask gpe69

--- nix/configurations/host/carbon/default.nix
+++ nix/configurations/host/carbon/default.nix
@@ -39,7 +39,7 @@
   # https://unix.stackexchange.com/questions/588018/kworker-thread-kacpid-notify-kacpid-hogging-60-70-of-cpu
   # https://askubuntu.com/questions/1275749/acpi-event-69-made-my-system-unusable
   boot.kernelParams = [
-    "acpi_mask_gpe=0x69" # very high count
+    # "acpi_mask_gpe=0x69" # very high count
 
     # This bricks bluetooth ?
     # "acpi_mask_gpe=0x16"
