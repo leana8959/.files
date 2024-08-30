2dccb42d62426649909c9297041de03b2fffd55d
Léana 江 <leana.jiang+git@icloud.com>
2024-08-30 16:58:14 +0200

    carbon: fixed nfs share params

--- nix/configurations/host/carbon/fs.nix
+++ nix/configurations/host/carbon/fs.nix
@@ -1,8 +1,8 @@
 {
-  # systemd.tmpfiles.rules = [
-  #   "d /mnt/data    0700 leana leana - -"
-  #   "d /mnt/seagate 0700 leana leana - -"
-  # ];
+  systemd.tmpfiles.rules = [
+    "d /mnt/data    0700 leana leana - -"
+    "d /mnt/seagate 0700 leana leana - -"
+  ];
 
   fileSystems."/mnt/data" = {
     device = "10.0.0.20:/mnt/mainPool/data";
@@ -10,10 +10,6 @@
     options = [
       "ro"
       "noauto"
-      "x-systemd.automount"
-      "x-systemd.idle-timeout=30min"
-      "x-systemd.device-timeout=5s"
-      "x-systemd.mount-timeout=5s"
     ];
   };
 }
