1bd2bd797765af7ad0053a64dad52c0dcb5f71bc
Léana 江 <leana.jiang+git@icloud.com>
2024-09-09 01:09:02 +0200

    carbon: update wallpaper

--- nix/configurations/home/carbon/wm.nix
+++ nix/configurations/home/carbon/wm.nix
@@ -2,6 +2,29 @@
 
 let
   inherit (pkgs) myPkgs;
+
+  wallpapers = {
+    nixos = pkgs.fetchurl {
+      url = "https://c.wallhere.com/photos/49/ce/Linux_Nixos_operating_system_minimalism-2175179.jpg!d";
+      hash = "sha256-RdrkvsVB6fHnyDZQ6wCGc7lUP7FoDYGvx2jfWB1WSvI=";
+    };
+
+    sequoia = pkgs.fetchurl {
+      url = "https://basicappleguy.com/s/SequoiaLight.png";
+      hash = "sha256-F3vo07kdE19luHtrBO8Q+Rj0mk+UlgtOSwOVdFW8Vlk=";
+    };
+
+    apple-pride-mac = pkgs.fetchurl {
+      url = "https://basicappleguy.com/s/Pride_Mac2.png";
+      hash = "sha256-PkGIlWf554Lk5e2gLDpDg+Rmb9qAniTiNvbV3/pppwA=";
+    };
+
+    apple-nebula-ipad = pkgs.fetchurl {
+      url = "https://basicappleguy.com/s/NebulaiPad.png";
+      hash = "sha256-rpWxhCH7eIT7ktRAROvL56eRK0T6AsgGE+iI/CLUF3k=";
+    };
+  };
+
 in
 
 {
@@ -15,7 +38,7 @@ in
 
   home.file = {
     ".xscreensaver".source = ./xscreensaver/.xscreensaver;
-    ".wallpaper".source = ./wallpapers/wp3023938-nebula-wallpapers-hd.jpg;
+    ".wallpaper".source = wallpapers.sequoia;
   };
 
   home.packages = [
