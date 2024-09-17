39132a612ce88980083ea122178f02fcb566c62f
Léana 江 <leana.jiang+git@icloud.com>
2024-09-17 23:13:26 +0200

    carbon: flash drive mount point

--- nix/configurations/host/carbon/fs.nix
+++ nix/configurations/host/carbon/fs.nix
@@ -2,6 +2,8 @@
   systemd.tmpfiles.rules = [
     "d /mnt/data    0700 leana leana - -"
     "d /mnt/seagate 0700 leana leana - -"
+
+    "d /home/leana/mnt/tdk32    0700 leana leana - -"
   ];
 
   fileSystems."/mnt/data" = {
@@ -10,6 +12,17 @@
     options = [
       "ro"
       "noauto"
+      "user"
+    ];
+  };
+
+  fileSystems."/home/leana/mnt/tdk32" = {
+    device = "/dev/disk/by-uuid/EF28-13EC";
+    fsType = "vfat";
+    options = [
+      "umask=0000"
+      "noauto"
+      "user"
     ];
   };
 }
