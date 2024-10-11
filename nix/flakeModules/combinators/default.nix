bb7e0eb54ecb3dbdd981adbd9f57f4b924b2dd9f
Léana 江 <leana.jiang+git@icloud.com>
2024-10-12 00:33:55 +0200

    nix: refactored flakeModules

--- nix/flakeModules/combinators/default.nix
+++ nix/flakeModules/combinators/default.nix
@@ -1,5 +1,61 @@
+{ inputs, withSystem, ... }:
+
+let
+  mkNixOS =
+    sharedModules:
+    {
+      hostname,
+      system,
+      modules ? [ ],
+    }:
+    inputs.nixpkgs.lib.nixosSystem {
+      specialArgs = {
+        inherit hostname;
+      };
+      modules = sharedModules { inherit hostname system; } ++ modules;
+    };
+
+  mkDarwin =
+    sharedModules:
+    {
+      hostname,
+      system,
+      modules ? [ ],
+    }:
+    inputs.nix-darwin.lib.darwinSystem {
+      specialArgs = {
+        inherit hostname;
+      };
+      modules = sharedModules { inherit hostname system; } ++ modules;
+    };
+
+  mkHomeManager =
+    sharedModules:
+    {
+      hostname,
+      system,
+      modules ? [ ],
+    }:
+    withSystem system (
+      { pkgs, ... }:
+      inputs.home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = {
+          inherit hostname;
+        };
+        modules = sharedModules { inherit hostname system; } ++ modules;
+      }
+    );
+
+  many = func: builtins.mapAttrs (hostname: cfgs: func (cfgs // { inherit hostname; }));
+in
 {
-  # Note: referencing to self would recurse infinitely.
-  imports = [ ./it.nix ];
-  flake.flakeModules.combinators = ./it.nix;
+  _module.args = {
+    inherit
+      many
+      mkNixOS
+      mkDarwin
+      mkHomeManager
+      ;
+  };
 }
--- nix/flakeModules/default.nix
+++ nix/flakeModules/default.nix
@@ -1,1 +1,13 @@
-{ imports = [ ./combinators ]; }
+{ lib, ... }:
+
+let
+  modules = lib.pipe (builtins.readDir ./.) [
+    (lib.filterAttrs (moduleName: _: moduleName != "default.nix"))
+    (lib.mapAttrs (moduleName: _: ./${moduleName}))
+  ];
+in
+
+{
+  imports = lib.attrValues modules;
+  flake.flakeModules = modules;
+}
